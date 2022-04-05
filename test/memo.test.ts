0531c8374f31dbe9443ded413619063e83d4bebb
Anurag Gupta <anuraggupta5086@gmail.com>
2022-04-06 03:25:11 +0530

    Added more tests

--- test/memo.test.ts
+++ test/memo.test.ts
@@ -6,18 +6,25 @@ import { TransactionInstruction } from '../src/transaction';
 import { Keypair } from '../src/keypair';
 import { PublicKey } from '../src/publickey';
 
-describe('Account', () => {
+describe('Memo Tests', () => {
   it('Return programId', () => {
     const programId = MemoProgram.id();
     
     expect(programId.toString()).to.eq('MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr');
   });
 
-  it('Check ProgramId', () => {
+  it('Check ProgramId Failure', () => {
     const newKey = new PublicKey('11111111111111111111111111111111')
     const idCheck = MemoProgram.checkId(newKey);
 
     expect(idCheck).to.eq(false);
+  });
+
+  it('Check ProgramId Success', () => {
+    const newKey = new PublicKey('MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr')
+    const idCheck = MemoProgram.checkId(newKey);
+
+    expect(idCheck).to.eq(true);
   });
 
   it('build memo', () => {
