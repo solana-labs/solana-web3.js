ec53acacad40e9bd0a878a6466bf2386e446acbe
Steven Luscher <steveluscher@users.noreply.github.com>
2023-03-09 19:50:42 -0800

    chore: mock `fetch` in tests (#1192)
    * chore: mock `fetch` in tests
    Summary: `jsdom` does not supply a polyfill for `fetch`; we have to do it ourselves
    
    * chore: create a build config that _only_ makes a browser build (#1193)
    
    Summary: We will need this to build browser-only forks of modules like `fetch-impl`.

---
+++ packages/build-scripts/tsup.config.browser.ts
@@ -0,0 +1,4 @@
+import { defineConfig } from 'tsup';
+import { getBaseConfig } from './getBaseConfig';
+
+export default defineConfig(options => [...getBaseConfig('browser', ['cjs', 'esm'], options)]);
