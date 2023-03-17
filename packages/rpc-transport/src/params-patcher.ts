7c306079ad9307a57ab8ff6b3c2f16b23fad9c9d
Steven Luscher <steven.luscher@solana.com>
2023-03-17 18:20:07 +0000

    [experimental] A handler you can supply to transports in the event of integer overflows ## Summary
    The JSON RPC was designed to communicate JavaScript `Numbers` over the wire, which puts values over `Number.MAX_SAFE_INTEGER` at risk of rounding errors. In this PR we add a hook that fires in the event of such an overflow. Implementers of that hook can decide to do whatever they want: throw, warn, or allow.
    ## Test Plan
    
    ```shell
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

--- packages/rpc-transport/src/params-patcher.ts
+++ packages/rpc-transport/src/params-patcher.ts
@@ -1,26 +1,36 @@
+type IntegerOverflowHandler = (keyPath: KeyPath, value: bigint) => void;
+type KeyPath = (number | string)[];
 type Patched<T> = T extends object ? { [Property in keyof T]: Patched<T[Property]> } : T extends bigint ? number : T;
-
 // FIXME(https://github.com/microsoft/TypeScript/issues/33014)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type TypescriptBug33014 = any;
 
-export function patchParamsForSolanaLabsRpc<T>(params: T): Patched<T> {
-    if (Array.isArray(params)) {
-        return params.map(patchParamsForSolanaLabsRpc) as TypescriptBug33014;
-    } else if (typeof params === 'object' && params !== null) {
+function visitNode<T>(value: T, keyPath: KeyPath, onIntegerOverflow?: IntegerOverflowHandler): Patched<T> {
+    if (Array.isArray(value)) {
+        return value.map((element, ii) =>
+            visitNode(element, [...keyPath, ii], onIntegerOverflow)
+        ) as TypescriptBug33014;
+    } else if (typeof value === 'object' && value !== null) {
         const out = {} as TypescriptBug33014;
-        for (const propName in params) {
-            if (Object.prototype.hasOwnProperty.call(params, propName)) {
-                out[propName] = patchParamsForSolanaLabsRpc(params[propName]);
+        for (const propName in value) {
+            if (Object.prototype.hasOwnProperty.call(value, propName)) {
+                out[propName] = visitNode(value[propName], [...keyPath, propName], onIntegerOverflow);
             }
         }
         return out as TypescriptBug33014;
-    } else if (typeof params === 'bigint') {
-        // TODO(solana-labs/solana/issues/30341) Create a data type to represent u64 in the Solana
+    } else if (typeof value === 'bigint') {
+        // FIXME(solana-labs/solana/issues/30341) Create a data type to represent u64 in the Solana
         // JSON RPC implementation so that we can throw away this entire patcher instead of unsafely
         // downcasting `bigints` to `numbers`.
-        return Number(params) as TypescriptBug33014;
+        if (onIntegerOverflow && value > Number.MAX_SAFE_INTEGER) {
+            onIntegerOverflow(keyPath, value);
+        }
+        return Number(value) as TypescriptBug33014;
     } else {
-        return params as TypescriptBug33014;
+        return value as TypescriptBug33014;
     }
+}
+
+export function patchParamsForSolanaLabsRpc<T>(params: T, onIntegerOverflow?: IntegerOverflowHandler): Patched<T> {
+    return visitNode(params, [], onIntegerOverflow);
 }
