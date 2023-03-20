ebffbba681cd408f8ac9d81fffef9d744616ca75
Steven Luscher <steveluscher@users.noreply.github.com>
2023-03-20 11:19:11 -0700

    [experimental] A handler you can supply to transports in the event of integer overflows (#1210)
    ## Summary
    
    The JSON RPC was designed to communicate JavaScript `Numbers` over the wire, which puts values over `Number.MAX_SAFE_INTEGER` at risk of rounding errors. In this PR we add a hook that fires in the event of such an overflow. Implementers of that hook can decide to do whatever they want: throw, warn, or allow.
    ## Test Plan
    
    ```shell
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

--- packages/rpc-transport/src/json-rpc-transport.ts
+++ packages/rpc-transport/src/json-rpc-transport.ts
@@ -5,6 +5,7 @@ import { createJsonRpcMessage } from './json-rpc-message';
 import { patchParamsForSolanaLabsRpc } from './params-patcher';
 
 type Config = Readonly<{
+    onIntegerOverflow?: (method: string, keyPath: (number | string)[], value: bigint) => void;
     url: string;
 }>;
 
@@ -12,10 +13,17 @@ type JsonRpcResponse<TResponse> = Readonly<
     { result: TResponse } | { error: { code: number; message: string; data?: unknown } }
 >;
 
-export function createJsonRpcTransport({ url }: Config): IJsonRpcTransport {
+export function createJsonRpcTransport({ onIntegerOverflow, url }: Config): IJsonRpcTransport {
     return {
         async send<TParams, TResponse>(method: string, params: TParams): Promise<TResponse> {
-            const patchedParams = patchParamsForSolanaLabsRpc(params);
+            const patchedParams = patchParamsForSolanaLabsRpc(
+                params,
+                onIntegerOverflow
+                    ? (keyPath, value) => {
+                          onIntegerOverflow(method, keyPath, value);
+                      }
+                    : undefined
+            );
             const jsonRpcMessage = createJsonRpcMessage(method, patchedParams);
             const response = await makeHttpRequest<JsonRpcResponse<TResponse>>({
                 payload: jsonRpcMessage,
