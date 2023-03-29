7124dd47232e855ac077685e5189d9c821b2de22
Steven Luscher <steveluscher@users.noreply.github.com>
2023-03-29 15:30:19 -0700

    [experimental] Mock transport instead of the underlying `fetch` primitive (#1222)
    ## Summary
    
    From the perspective of the RPC transport, it's not interesting that the wire transport uses `fetch`. All that's interesting is that the wire transport returns responses.
    
    In this PR, we mock at the wire transport level, not at the underlying `fetch` API level. The `fetch` API level is already well tested by `http-transport-test.ts`
    
    ## Test Plan
    
    ```
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

--- packages/rpc-transport/src/__tests__/json-rpc-transport-test.ts
+++ packages/rpc-transport/src/__tests__/json-rpc-transport-test.ts
@@ -1,23 +1,30 @@
 import { IJsonRpcTransport } from '..';
+import { makeHttpRequest } from '../http-request';
 import { SolanaJsonRpcError } from '../json-rpc-errors';
 import { createJsonRpcTransport } from '../json-rpc-transport';
 
-import fetchMock from 'jest-fetch-mock';
+jest.mock('../http-request');
 
 describe('JSON-RPC 2.0 transport', () => {
     let transport: IJsonRpcTransport;
     beforeEach(() => {
         transport = createJsonRpcTransport({ url: 'fake://url' });
+        (makeHttpRequest as jest.Mock).mockImplementation(
+            () =>
+                new Promise(_ => {
+                    /* never resolve */
+                })
+        );
     });
     it('returns results from a JSON-RPC 2.0 endpoint', async () => {
         expect.assertions(1);
-        fetchMock.once(JSON.stringify({ result: 123 }));
+        (makeHttpRequest as jest.Mock).mockResolvedValueOnce({ result: 123 });
         const result = await transport.send('someMethod', undefined);
         expect(result).toBe(123);
     });
     it('throws errors from a JSON-RPC 2.0 endpoint', async () => {
         expect.assertions(3);
-        fetchMock.once(JSON.stringify({ error: { code: 123, data: 'abc', message: 'o no' } }));
+        (makeHttpRequest as jest.Mock).mockResolvedValueOnce({ error: { code: 123, data: 'abc', message: 'o no' } });
         const sendPromise = transport.send('someMethod', undefined);
         await expect(sendPromise).rejects.toThrow(SolanaJsonRpcError);
         await expect(sendPromise).rejects.toThrow(/o no/);
