af1d4c1fd037e4600ff7aca3c675972e05375490
steveluscher <me+github@steveluscher.com>
2023-04-03 22:39:46 +0000

    test: ensure that the test validator starter always gets a lease on the validator, even if one is already running

--- packages/test-config/test-validator-setup.js
+++ packages/test-config/test-validator-setup.js
@@ -2,12 +2,18 @@
 const { setup } = require('jest-dev-server');
 
 module.exports = async function globalSetup() {
-    globalThis.servers = await setup({
-        command: '../../scripts/start-shared-test-validator.sh',
-        host: '127.0.0.1',
-        launchTimeout: 50000,
-        path: 'health',
-        port: 8899,
-        protocol: 'http',
-    });
+    globalThis.servers = await setup([
+        // Unconditionally obtain a lease on the test validator.
+        { command: '../../scripts/start-shared-test-validator.sh' },
+        // This 'server' is a noop; we only use it to run the 'wait for server' logic.
+        {
+            command: 'sleep infinity',
+            host: '127.0.0.1',
+            launchTimeout: 50000,
+            path: 'health',
+            port: 8899,
+            protocol: 'http',
+            usedPortAction: 'ignore',
+        },
+    ]);
 };
