304f450743e2d3b0ddd8910c7c1f26c7b3aa5955
steveluscher <me+github@steveluscher.com>
2023-04-05 16:44:03 +0000

    chore: replace `@noble/secp256k1` with `@noble/curves/secp256k1`

--- packages/library-legacy/src/utils/secp256k1.ts
+++ packages/library-legacy/src/utils/secp256k1.ts
@@ -1,18 +1,11 @@
-import {hmac} from '@noble/hashes/hmac';
-import {sha256} from '@noble/hashes/sha256';
-import * as secp256k1 from '@noble/secp256k1';
-
-// Supply a synchronous hashing algorithm to make this
-// library interoperable with the synchronous APIs in web3.js.
-secp256k1.utils.hmacSha256Sync = (key: Uint8Array, ...msgs: Uint8Array[]) => {
-  const h = hmac.create(sha256, key);
-  msgs.forEach(msg => h.update(msg));
-  return h.digest();
-};
+import {secp256k1} from '@noble/curves/secp256k1';
 
 export const ecdsaSign = (
-  msgHash: Parameters<typeof secp256k1.signSync>[0],
-  privKey: Parameters<typeof secp256k1.signSync>[1],
-) => secp256k1.signSync(msgHash, privKey, {der: false, recovered: true});
+  msgHash: Parameters<typeof secp256k1.sign>[0],
+  privKey: Parameters<typeof secp256k1.sign>[1],
+) => {
+  const signature = secp256k1.sign(msgHash, privKey);
+  return [signature.toCompactRawBytes(), signature.recovery!] as const;
+};
 export const isValidPrivateKey = secp256k1.utils.isValidPrivateKey;
 export const publicKeyCreate = secp256k1.getPublicKey;
