92a60c3fe72e10004b8f4e9783a9425e68668bb4
Steven Luscher <steven.luscher@solana.com>
2023-04-18 19:56:58 +0000

    refactor(experimental): rewrite library rpc header tests not to rely on implementation details

--- packages/library/src/__tests__/rpc-header-config-test.ts
+++ packages/library/src/__tests__/rpc-header-config-test.ts
@@ -1,29 +1,16 @@
 import { createDefaultRpc } from '../rpc';
 
-import fetchMock from 'jest-fetch-mock-fork';
+import { createJsonRpcTransport } from '@solana/rpc-transport';
 
-const FAKE_URL = 'fake://url';
+jest.mock('@solana/rpc-transport');
 
 describe('RPC custom HTTP header config', () => {
-    let transport: ReturnType<typeof createDefaultRpc>;
-    beforeEach(() => {
-        transport = createDefaultRpc({
-            headers: {
-                aUtHoRiZaTiOn: 'Picard, 4 7 Alpha Tango',
-                'sOlAnA-cLiEnT': 'fakeversion',
-            },
-            url: FAKE_URL,
-        });
-        fetchMock.mockReturnValue(
-            new Promise(() => {
-                /* never resolve */
-            })
-        );
-        transport.getBlockHeight().send();
-    });
     it('adds configured headers to each request with downcased property names', () => {
-        expect(fetchMock).toHaveBeenCalledWith(
-            expect.anything(),
+        createDefaultRpc({
+            headers: { aUtHoRiZaTiOn: 'Picard, 4 7 Alpha Tango' },
+            url: 'fake://url',
+        });
+        expect(createJsonRpcTransport).toHaveBeenCalledWith(
             expect.objectContaining({
                 headers: expect.objectContaining({
                     authorization: 'Picard, 4 7 Alpha Tango',
@@ -31,9 +18,9 @@ describe('RPC custom HTTP header config', () => {
             })
         );
     });
-    it('adds a `solana-client` header with the current NPM package version', () => {
-        expect(fetchMock).toHaveBeenCalledWith(
-            expect.anything(),
+    it('adds a `solana-client` header with the current NPM package version by default', () => {
+        createDefaultRpc({ url: 'fake://url' });
+        expect(createJsonRpcTransport).toHaveBeenCalledWith(
             expect.objectContaining({
                 headers: expect.objectContaining({
                     // Version is defined in `setup-define-version-constant.ts`
@@ -43,8 +30,11 @@ describe('RPC custom HTTP header config', () => {
         );
     });
     it('makes it impossible to override the `solana-client` header', () => {
-        expect(fetchMock).toHaveBeenCalledWith(
-            expect.anything(),
+        createDefaultRpc({
+            headers: { 'sOlAnA-cLiEnT': 'fakeversion' },
+            url: 'fake://url',
+        });
+        expect(createJsonRpcTransport).toHaveBeenCalledWith(
             expect.objectContaining({
                 headers: expect.objectContaining({
                     // Version is defined in `setup-define-version-constant.ts`
@@ -52,8 +42,7 @@ describe('RPC custom HTTP header config', () => {
                 }),
             })
         );
-        expect(fetchMock).toHaveBeenCalledWith(
-            expect.anything(),
+        expect(createJsonRpcTransport).toHaveBeenCalledWith(
             expect.objectContaining({
                 headers: expect.not.objectContaining({
                     'sOlAnA-cLiEnT': 'fakeversion',
