44f2cfc50ac16e38dee731b1704c47e0d5bca591
Steven Luscher <steven.luscher@solana.com>
2023-04-21 00:36:44 +0000

    chore: pr to test bundlesize

--- packages/library/src/rpc.ts
+++ packages/library/src/rpc.ts
@@ -4,6 +4,16 @@ import { DEFAULT_RPC_CONFIG } from './rpc-default-config';
 import { createJsonRpc } from '@solana/rpc-transport';
 import type { Rpc } from '@solana/rpc-transport/dist/types/json-rpc-types';
 
+export const thingThatTakesUpSpace = `
+__
+___                  ___                       ( )
+|""|  ___    _   __  |"""|  __                   \`
+|""| |"""|  |"| |""| |"""| |""|        _._ _
+|""| |"""|  |"| |""| |"""| |""|       (__((_(
+|""| |"""|  |"| |""| |"""| |""|      \\'-:--:-.
+"'''"''"'""'"""''"''''"""'""'""'~~~~~~'-----'~~~~  ldb
+`;
+
 export function createSolanaRpc(config: Omit<Parameters<typeof createJsonRpc>[0], 'api'>): Rpc<SolanaRpcMethods> {
     return createJsonRpc<SolanaRpcMethods>({
         ...config,
