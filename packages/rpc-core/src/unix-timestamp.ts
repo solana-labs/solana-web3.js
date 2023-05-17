88723edfa4edb0b007bf011d5b7927ccd2862819
Steven Luscher <steven.luscher@solana.com>
2023-05-17 18:49:48 +0000

    refactor(experimental): update timestamp assertion error message

--- packages/rpc-core/src/unix-timestamp.ts
+++ packages/rpc-core/src/unix-timestamp.ts
@@ -4,7 +4,7 @@ export function assertIsUnixTimestamp(putativeTimestamp: number): asserts putati
     // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#the_epoch_timestamps_and_invalid_date
     try {
         if (putativeTimestamp > 8.64e15 || putativeTimestamp < -8.64e15) {
-            throw new Error('Expected input number to be less than or equal to 8.64e15');
+            throw new Error('Expected input number to be in the range [-8.64e15, 8.64e15]');
         }
     } catch (e) {
         throw new Error(`\`${putativeTimestamp}\` is not a timestamp`, {
