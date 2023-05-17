caf8d5116ba35dcd643c64c75b8e71af8f99fdef
Callum McIntyre <mcintyre1994@gmail.com>
2023-05-17 17:35:28 +0100

    refactor(experimental): add the `getEpochInfo` RPC method (#1299)
    Co-authored-by: Callum McIntyre <callum.mcintyre@solana.com>

---
+++ packages/rpc-core/src/rpc-methods/__tests__/get-epoch-info-test.ts
@@ -0,0 +1,51 @@
+import { createHttpTransport, createJsonRpc } from '@solana/rpc-transport';
+import type { SolanaJsonRpcErrorCode } from '@solana/rpc-transport/dist/types/json-rpc-errors';
+import type { Rpc } from '@solana/rpc-transport/dist/types/json-rpc-types';
+import fetchMock from 'jest-fetch-mock-fork';
+import { createSolanaRpcApi, SolanaRpcMethods } from '../index';
+import { Commitment } from '../common';
+
+describe('getEpochInfo', () => {
+    let rpc: Rpc<SolanaRpcMethods>;
+    beforeEach(() => {
+        fetchMock.resetMocks();
+        fetchMock.dontMock();
+        rpc = createJsonRpc<SolanaRpcMethods>({
+            api: createSolanaRpcApi(),
+            transport: createHttpTransport({ url: 'http://127.0.0.1:8899' }),
+        });
+    });
+
+    (['confirmed', 'finalized', 'processed'] as Commitment[]).forEach(commitment => {
+        describe(`when called with \`${commitment}\` commitment`, () => {
+            it('returns epoch info', async () => {
+                expect.assertions(1);
+                const epochInfoPromise = rpc.getEpochInfo().send();
+                await expect(epochInfoPromise).resolves.toMatchObject({
+                    absoluteSlot: expect.any(BigInt),
+                    blockHeight: expect.any(BigInt),
+                    epoch: expect.any(BigInt),
+                    slotIndex: expect.any(BigInt),
+                    slotsInEpoch: expect.any(BigInt),
+                    transactionCount: expect.any(BigInt),
+                });
+            });
+        });
+    });
+
+    describe('when called with a `minContextSlot` higher than the highest slot available', () => {
+        it('throws an error', async () => {
+            expect.assertions(1);
+            const epochInfoPromise = rpc
+                .getEpochInfo({
+                    minContextSlot: 2n ** 63n - 1n, // u64:MAX; safe bet it'll be too high.
+                })
+                .send();
+            await expect(epochInfoPromise).rejects.toMatchObject({
+                code: -32016 satisfies (typeof SolanaJsonRpcErrorCode)['JSON_RPC_SERVER_ERROR_MIN_CONTEXT_SLOT_NOT_REACHED'],
+                message: expect.any(String),
+                name: 'SolanaJsonRpcError',
+            });
+        });
+    });
+});
