b8df7b400586e6cbbb1ec807ea698f2a66e81aa3
Steven Luscher <steveluscher@users.noreply.github.com>
2023-06-09 16:25:53 -0700

    refactor(experimental): types for transaction instructions (#1302)
    ## Summary
    
    This is a first crack at types that you can use to statically constrain transaction instructions.
    
    ## Test Plan
    
    You can use `IAccountMeta` to constrain the types of accounts something accepts.
    
    ```ts
    type RentSysvar = ReadableAccount<'SysvarRent111111111111111111111111111111111'>;
    
    const rentSysvarAddress = 'SysvarRent111111111111111111111111111111111';
    assertIsBase58EncodedAddress(rentSysvarAddress);
    
    const rent: RentSysvar = {
      address: rentSysvarAddress,
      isSigner: false,
      isWritable: false,
    };
    ```
    
    You can use `Instruction` to constrain the type of a particular instruction..
    
    ```ts
    type InitializeStakeInstruction = Instruction<
        // Program address
        'StakeConfig11111111111111111111111111111111',
        // Accounts
        [
            WritableAccount, // Stake account
            RentSysvar
        ],
        // Data
        InitializeStakeInstructionData
    >;
    
    const stakeConfigProgramAddress = 'StakeConfig11111111111111111111111111111111';
    assertIsBase58EncodedAddress(stakeConfigProgramAddress);
    
    const initializeStakeInstruction: InitializeStakeInstruction = {
      programAddress: stakeConfigProgramAddress,
      accounts: [
        {address: stakeAccountAddress, isSigner: false, isWritable: true},
        rent,
      ],
      data: /* ... */
    };
    ```

---
+++ packages/instructions/src/instruction.ts
@@ -0,0 +1,18 @@
+import { Base58EncodedAddress } from '@solana/keys';
+
+import { IAccountLookupMeta, IAccountMeta } from './accounts';
+
+export interface IInstruction<TProgramAddress extends string = string> {
+    readonly accounts?: readonly (IAccountMeta | IAccountLookupMeta)[];
+    readonly data?: Uint8Array;
+    readonly programAddress: Base58EncodedAddress<TProgramAddress>;
+}
+
+export interface IInstructionWithAccounts<TAccounts extends readonly (IAccountMeta | IAccountLookupMeta)[]>
+    extends IInstruction {
+    readonly accounts: TAccounts;
+}
+
+export interface IInstructionWithData<TData extends Uint8Array> extends IInstruction {
+    readonly data: TData;
+}
--- packages/library/src/index.ts
+++ packages/library/src/index.ts
@@ -1,3 +1,4 @@
+export * from '@solana/instructions';
 export * from '@solana/keys';
 export * from './rpc';
 export * from './rpc-transport';
