04619af8e9ac2527ec94dedccec6cb2ef9f7b70c
Steven Luscher <steven.luscher@solana.com>
2023-06-20 19:25:48 +0000

    refactor(experimental): a helper you can use to set the fee payer on a transaction ## Summary This helper both sets the fee payer on a transaction and clears the signatures if the fee payer has changed. If the fee payer has _not_ changed it returns the identical object without mutation.
    ## Test Plan
    
    ```
    cd packages/transactions
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

--- packages/transactions/src/index.ts
+++ packages/transactions/src/index.ts
@@ -1,5 +1,6 @@
 export * from './blockhash';
 export * from './create-transaction';
 export * from './durable-nonce';
+export * from './fee-payer';
 export * from './signatures';
 export * from './types';
