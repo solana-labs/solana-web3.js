e7a27fbfb1bb3cc0b7dc7ae7b9bc92cb5b0607c3
Steven Luscher <steveluscher@users.noreply.github.com>
2023-06-28 16:24:08 -0700

    refactor(experimental): a function to compile a transaction's address table lookups into a message (#1365)
    * refactor(experimental): a function to compile a transaction's instructions into a message
    ## Summary
    
    Over the wire, transaction instructions don't carry the addresses that they refer to, but rather the _indices_ of those addresses in a sidecar address table. This function takes in the ordered addresses and the instructions, and returns the instructions with the addresses replaced with indices.
    
    
    ## Test Plan
    
    ```
    cd packages/transactions/
    pnpm test:unit:browser
    pnpm test:unit:node
    ```
    
    * refactor(experimental): a function to compile a transaction's static accounts into a message
    ## Summary
    
    In this PR we simply map over the ordered accounts and output the address associated with each, but only for static accounts.
    
    ## Test Plan
    
    ```
    cd packages/transactions/
    pnpm test:unit:browser
    pnpm test:unit:node
    ```
    
    * refactor(experimental): a function to compile a transaction's address table lookups into a message
    ## Summary
    
    Once we have an ordered list of accounts, producing the address table lookups for a transaction is a matter of rolling them up into a `Vec<MessageAddressTableLookup>`. This PR implements that.
    
    ## Test Plan
    
    ```
    cd packages/transactions/
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

---
+++ packages/transactions/src/__tests__/compile-address-table-lookups-test.ts
@@ -0,0 +1,100 @@
+import { AccountRole } from '@solana/instructions';
+import { Base58EncodedAddress, getBase58EncodedAddressComparator } from '@solana/keys';
+
+import { OrderedAccounts } from '../accounts';
+import { getCompiledAddressTableLookups } from '../compile-address-table-lookups';
+
+const MOCK_ADDRESSES: ReadonlyArray<Base58EncodedAddress> = [
+    'BRwZRKsvKkG45g59269qZ5e8UaECFim5Qfxex44UKwDG',
+    'AZE3mXbzNp8SfZYBfL4L67ejQ8zmatAKKezUdCarKnUL',
+    'Awft9caFzun5FcVTaXJAkAYDBgbEDF5QALeaqeY3M3Va',
+    'ARc8zz6T14LZQTpjryhBGDuNZb3YmNT9PtEuBSuVM5xo',
+    '6Tu9wk1r9yGwzd3xdrVzDttmkTN98iiffq7L25JKTvwh',
+    '4R6dgeBbwPjnTSN78KGxhB78oFsxaobiwBsCBLAyauqA',
+] as Base58EncodedAddress[];
+
+let _nextMockAddress = 0;
+function getMockAddress() {
+    return `${_nextMockAddress++}` as Base58EncodedAddress;
+}
+
+describe('getCompiledAddressTableLookups', () => {
+    it('returns address table lookups in lexical order', () => {
+        const sortedAddresses = [...MOCK_ADDRESSES].sort(getBase58EncodedAddressComparator());
+        const orderedAccounts = [
+            {
+                address: getMockAddress(),
+                addressIndex: 10,
+                lookupTableAddress: sortedAddresses[1],
+                role: AccountRole.WRITABLE,
+            },
+            {
+                address: getMockAddress(),
+                addressIndex: 20,
+                lookupTableAddress: sortedAddresses[2],
+                role: AccountRole.WRITABLE,
+            },
+            {
+                address: getMockAddress(),
+                addressIndex: 30,
+                lookupTableAddress: sortedAddresses[0],
+                role: AccountRole.READONLY,
+            },
+            {
+                address: getMockAddress(),
+                addressIndex: 40,
+                lookupTableAddress: sortedAddresses[3],
+                role: AccountRole.READONLY,
+            },
+            {
+                address: getMockAddress(),
+                addressIndex: 50,
+                lookupTableAddress: sortedAddresses[2],
+                role: AccountRole.READONLY,
+            },
+        ] as OrderedAccounts;
+        const compiledAddressTableLookups = getCompiledAddressTableLookups(orderedAccounts);
+        expect(compiledAddressTableLookups).toHaveProperty('0.lookupTableAddress', sortedAddresses[0]);
+        expect(compiledAddressTableLookups).toHaveProperty('1.lookupTableAddress', sortedAddresses[1]);
+        expect(compiledAddressTableLookups).toHaveProperty('2.lookupTableAddress', sortedAddresses[2]);
+        expect(compiledAddressTableLookups).toHaveProperty('3.lookupTableAddress', sortedAddresses[3]);
+    });
+    it('populates readable/writable address indices in order', () => {
+        const lookupTableAddress = getMockAddress();
+        const orderedAccounts = [
+            {
+                address: getMockAddress(),
+                addressIndex: 20,
+                lookupTableAddress,
+                role: AccountRole.WRITABLE,
+            },
+            {
+                address: getMockAddress(),
+                addressIndex: 10,
+                lookupTableAddress,
+                role: AccountRole.WRITABLE,
+            },
+            {
+                address: getMockAddress(),
+                addressIndex: 30,
+                lookupTableAddress,
+                role: AccountRole.READONLY,
+            },
+            {
+                address: getMockAddress(),
+                addressIndex: 50,
+                lookupTableAddress,
+                role: AccountRole.READONLY,
+            },
+            {
+                address: getMockAddress(),
+                addressIndex: 40,
+                lookupTableAddress,
+                role: AccountRole.READONLY,
+            },
+        ] as OrderedAccounts;
+        const compiledAddressTableLookups = getCompiledAddressTableLookups(orderedAccounts);
+        expect(compiledAddressTableLookups).toHaveProperty('0.readableIndices', [30, 50, 40]);
+        expect(compiledAddressTableLookups).toHaveProperty('0.writableIndices', [20, 10]);
+    });
+});
