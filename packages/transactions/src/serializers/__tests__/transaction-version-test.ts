491e16ae825c942644d49c22a78de8a6359de50f
Steven Luscher <steven.luscher@solana.com>
2023-06-28 23:26:35 +0000

    refactor(experimental): set up a pattern for splitting the serializers into co/dec/codec slices ## Summary
    Most use cases for these serializers will only ever need to encode or decode, never both. This PR sets up a standard for splitting serializers into a version that encodes, decodes, or does both. Import the one you need, and let your build system tree-shake the other one away.
    
    ## Test Plan
    
    ```
    cd packages/transactions/
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

--- packages/transactions/src/serializers/__tests__/transaction-version-test.ts
+++ packages/transactions/src/serializers/__tests__/transaction-version-test.ts
@@ -1,31 +1,71 @@
+import { Serializer } from '@metaplex-foundation/umi-serializers';
+
 import { TransactionVersion } from '../../types';
-import { transactionVersionHeader } from '../transaction-version';
+import {
+    getTransactionVersionCodec,
+    getTransactionVersionDecoder,
+    getTransactionVersionEncoder,
+} from '../transaction-version';
 
 const VERSION_FLAG_MASK = 0x80;
 const VERSION_TEST_CASES = // Versions 0–127
     [...Array(128).keys()].map(version => [version | VERSION_FLAG_MASK, version as TransactionVersion] as const);
-describe('Transaction version serializer', () => {
-    it('serializes no data when the version is `legacy`', () => {
-        expect(transactionVersionHeader.serialize('legacy')).toEqual(new Uint8Array());
+
+describe.each([getTransactionVersionCodec, getTransactionVersionEncoder])(
+    'Transaction version serializer',
+    serializerFactory => {
+        let transactionVersion: Serializer<TransactionVersion>;
+        beforeEach(() => {
+            transactionVersion = serializerFactory();
+        });
+        it('serializes no data when the version is `legacy`', () => {
+            expect(transactionVersion.serialize('legacy')).toEqual(new Uint8Array());
+        });
+        it.each(VERSION_TEST_CASES)('serializes to `%s` when the version is `%s`', (expected, version) => {
+            expect(transactionVersion.serialize(version)).toEqual(new Uint8Array([expected]));
+        });
+        it.each([-1 as TransactionVersion, 128 as TransactionVersion])(
+            'throws when passed the out-of-range version `%s`',
+            version => {
+                expect(() => transactionVersion.serialize(version)).toThrow();
+            }
+        );
+    }
+);
+
+describe.each([getTransactionVersionCodec, getTransactionVersionDecoder])(
+    'Transaction version deserializer',
+    serializerFactory => {
+        let transactionVersion: Serializer<TransactionVersion>;
+        beforeEach(() => {
+            transactionVersion = serializerFactory();
+        });
+        it.each(VERSION_TEST_CASES)('deserializes `%s` to the version `%s`', (byte, expected) => {
+            expect(transactionVersion.deserialize(new Uint8Array([byte]))[0]).toEqual(expected);
+        });
+        it('deserializes to `legacy` when missing the version flag', () => {
+            expect(
+                transactionVersion.deserialize(
+                    // eg. just a byte that indicates that there are 3 required signers
+                    new Uint8Array([3])
+                )[0]
+            ).toBe('legacy');
+        });
+    }
+);
+
+describe('The transaction version decode-only factory', () => {
+    it('throws when you call `serialize`', () => {
+        expect(getTransactionVersionDecoder().serialize).toThrowErrorMatchingInlineSnapshot(
+            `"No encoder exists for TransactionVersion. Use \`getTransactionVersionEncoder()\` if you need a encoder, and \`getTransactionVersionCodec()\` if you need to both encode and decode TransactionVersion"`
+        );
     });
-    it.each(VERSION_TEST_CASES)('serializes to `%s` when the version is `%s`', (expected, version) => {
-        expect(transactionVersionHeader.serialize(version)).toEqual(new Uint8Array([expected]));
+});
+
+describe('The transaction version encode-only factory', () => {
+    it('throws when you call `deserialize`', () => {
+        expect(getTransactionVersionEncoder().deserialize).toThrowErrorMatchingInlineSnapshot(
+            `"No decoder exists for TransactionVersion. Use \`getTransactionVersionDecoder()\` if you need a decoder, and \`getTransactionVersionCodec()\` if you need to both encode and decode TransactionVersion"`
+        );
     });
-    it.each(VERSION_TEST_CASES)('deserializes `%s` to the version `%s`', (byte, expected) => {
-        expect(transactionVersionHeader.deserialize(new Uint8Array([byte]))[0]).toEqual(expected);
-    });
-    it('deserializes to `legacy` when missing the version flag', () => {
-        expect(
-            transactionVersionHeader.deserialize(
-                // eg. just a byte that indicates that there are 3 required signers
-                new Uint8Array([3])
-            )[0]
-        ).toBe('legacy');
-    });
-    it.each([-1 as TransactionVersion, 128 as TransactionVersion])(
-        'throws when passed the out-of-range version `%s`',
-        version => {
-            expect(() => transactionVersionHeader.serialize(version)).toThrow();
-        }
-    );
 });
--- packages/transactions/src/serializers/transaction-version.ts
+++ packages/transactions/src/serializers/transaction-version.ts
@@ -1,29 +1,58 @@
+import { Serializer } from '@metaplex-foundation/umi-serializers';
+
 import { TransactionVersion } from '../types';
+import { getUnimplementedDecoder, getUnimplementedEncoder } from './unimplemented';
 
 const VERSION_FLAG_MASK = 0x80;
 
-export const transactionVersionHeader = {
+const BASE_CONFIG = {
     description: __DEV__ ? 'A single byte that encodes the version of the transaction' : '',
-    deserialize: (bytes: Uint8Array, offset = 0): [TransactionVersion, number] => {
-        const firstByte = bytes[offset];
-        if ((firstByte & VERSION_FLAG_MASK) === 0) {
-            // No version flag set; it's a legacy (unversioned) transaction.
-            return ['legacy', offset];
-        } else {
-            const version = (firstByte ^ VERSION_FLAG_MASK) as TransactionVersion;
-            return [version, offset + 1];
-        }
-    },
     fixedSize: null,
     maxSize: 1,
-    serialize: (value: TransactionVersion): Uint8Array => {
-        if (value === 'legacy') {
-            return new Uint8Array();
-        }
-        if (value < 0 || value > 127) {
-            // TODO: Coded error.
-            throw new Error(`Transaction version must be in the range [0, 127]. \`${value}\` given.`);
-        }
-        return new Uint8Array([value | VERSION_FLAG_MASK]);
-    },
-};
+} as const;
+
+function deserialize(bytes: Uint8Array, offset = 0): [TransactionVersion, number] {
+    const firstByte = bytes[offset];
+    if ((firstByte & VERSION_FLAG_MASK) === 0) {
+        // No version flag set; it's a legacy (unversioned) transaction.
+        return ['legacy', offset];
+    } else {
+        const version = (firstByte ^ VERSION_FLAG_MASK) as TransactionVersion;
+        return [version, offset + 1];
+    }
+}
+
+function serialize(value: TransactionVersion): Uint8Array {
+    if (value === 'legacy') {
+        return new Uint8Array();
+    }
+    if (value < 0 || value > 127) {
+        // TODO: Coded error.
+        throw new Error(`Transaction version must be in the range [0, 127]. \`${value}\` given.`);
+    }
+    return new Uint8Array([value | VERSION_FLAG_MASK]);
+}
+
+export function getTransactionVersionDecoder(): Serializer<TransactionVersion> {
+    return {
+        ...BASE_CONFIG,
+        deserialize,
+        serialize: getUnimplementedEncoder('TransactionVersion'),
+    };
+}
+
+export function getTransactionVersionEncoder(): Serializer<TransactionVersion> {
+    return {
+        ...BASE_CONFIG,
+        deserialize: getUnimplementedDecoder('TransactionVersion'),
+        serialize,
+    };
+}
+
+export function getTransactionVersionCodec(): Serializer<TransactionVersion> {
+    return {
+        ...BASE_CONFIG,
+        deserialize,
+        serialize,
+    };
+}
