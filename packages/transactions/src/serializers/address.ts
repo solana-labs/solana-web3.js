02a66efe0aebe4c6346589f91c7d1767eb0f8481
Steven Luscher <steveluscher@users.noreply.github.com>
2023-06-29 12:17:48 -0700

    refactor(experimental): a seriailzer for address table lookups (#1373)
    ## Summary
    
    An address table lookup forms part of v0 transactions. Each one contains the address of the lookup table account itself, the indices of the accounts within that should be write-locked, and the indices of the accounts that should be read-only.
    
    ## Test Plan
    
    ```
    cd packages/transactions/
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

--- packages/transactions/src/serializers/address.ts
+++ packages/transactions/src/serializers/address.ts
@@ -1,9 +1,13 @@
 import { base58, Serializer, string } from '@metaplex-foundation/umi-serializers';
 import { Base58EncodedAddress } from '@solana/keys';
 
-export function getBase58EncodedAddressCodec(): Serializer<Base58EncodedAddress> {
+type Config = Readonly<{
+    description: string;
+}>;
+
+export function getBase58EncodedAddressCodec(config?: Config): Serializer<Base58EncodedAddress> {
     return string({
-        description: __DEV__ ? 'A 32-byte account address' : '',
+        description: config?.description ?? (__DEV__ ? 'A 32-byte account address' : ''),
         encoding: base58,
         size: 32,
     }) as unknown as Serializer<Base58EncodedAddress>;
