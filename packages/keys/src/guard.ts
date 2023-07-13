75a0c81801619678bdaba7c13602288e8f2b60da
Steven Luscher <steven.luscher@solana.com>
2023-07-13 18:16:27 +0000

    refactor(experimental): assertion functions that you can use to determine whether Ed25519 is supported
    ## Summary
    
    It's just nice to have these well tested, and all in one place. These will only become more complicated, so it's best to get them out of the code.
    
    Only thing is, I wish that TypeScript could assert about globals: https://stackoverflow.com/q/76657623/802047
    
    ## Test Plan
    
    ```
    cd packages/keys/
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

---
+++ packages/keys/src/guard.ts
@@ -0,0 +1,66 @@
+function assertIsSecureContext() {
+    if (__BROWSER__ && !globalThis.isSecureContext) {
+        // TODO: Coded error.
+        throw new Error(
+            'Cryptographic operations are only allowed in secure browser contexts. Read more ' +
+                'here: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts'
+        );
+    }
+}
+
+let cachedEd25519Decision: PromiseLike<boolean> | boolean | undefined;
+async function isEd25519CurveSupported(subtle: SubtleCrypto): Promise<boolean> {
+    if (cachedEd25519Decision === undefined) {
+        cachedEd25519Decision = new Promise(resolve => {
+            subtle
+                .generateKey('Ed25519', /* extractable */ false, ['sign', 'verify'])
+                .catch(() => {
+                    resolve((cachedEd25519Decision = false));
+                })
+                .then(() => {
+                    resolve((cachedEd25519Decision = true));
+                });
+        });
+    }
+    if (typeof cachedEd25519Decision === 'boolean') {
+        return cachedEd25519Decision;
+    } else {
+        return await cachedEd25519Decision;
+    }
+}
+
+export async function assertKeyGenerationIsAvailable() {
+    assertIsSecureContext();
+    if (typeof globalThis.crypto === 'undefined' || typeof globalThis.crypto.subtle?.generateKey !== 'function') {
+        // TODO: Coded error.
+        throw new Error('No key generation implementation could be found');
+    }
+    if (!(await isEd25519CurveSupported(globalThis.crypto.subtle))) {
+        // TODO: Coded error.
+        throw new Error('This runtime does not support the generation of Ed25519 keypairs');
+    }
+}
+
+export async function assertKeyExporterIsAvailable() {
+    assertIsSecureContext();
+    if (typeof globalThis.crypto === 'undefined' || typeof globalThis.crypto.subtle?.exportKey !== 'function') {
+        // TODO: Coded error.
+        throw new Error('No key export implementation could be found');
+    }
+}
+
+export async function assertSigningCapabilityIsAvailable() {
+    assertIsSecureContext();
+    if (typeof globalThis.crypto === 'undefined' || typeof globalThis.crypto.subtle?.sign !== 'function') {
+        // TODO: Coded error.
+        throw new Error('No signing implementation could be found');
+    }
+}
+
+export async function assertVerificationCapabilityIsAvailable() {
+    assertIsSecureContext();
+    if (typeof globalThis.crypto === 'undefined' || typeof globalThis.crypto.subtle?.verify !== 'function') {
+        // TODO: Coded error.
+        throw new Error('No signature verification implementation could be found');
+    }
+}
