9bf9fdbb1fd7bdbf277cba4940591fb16ae4558f
Steven Luscher <steveluscher@users.noreply.github.com>
2023-07-14 16:18:46 -0700

    refactor(experimental): assertion functions that you can use to determine whether Ed25519 is supported (#1393)
    * refactor(experimental): assertion functions that you can use to determine whether Ed25519 is supported
    
    ## Summary
    
    It's just nice to have these well tested, and all in one place. These will only become more complicated, so it's best to get them out of the code.
    
    Only thing is, I wish that TypeScript could assert about globals: https://stackoverflow.com/q/76657623/802047
    
    ## Test Plan
    
    ```
    cd packages/keys/
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

---
+++ packages/keys/src/__tests__/guard-test.ts
@@ -0,0 +1,215 @@
+import {
+    assertKeyExporterIsAvailable,
+    assertSigningCapabilityIsAvailable,
+    assertVerificationCapabilityIsAvailable,
+} from '../guard';
+
+describe('assertKeyExporterIsAvailable()', () => {
+    let cachedIsSecureContext: boolean;
+    beforeEach(async () => {
+        cachedIsSecureContext = globalThis.isSecureContext;
+        globalThis.isSecureContext = true;
+    });
+    afterEach(() => {
+        globalThis.isSecureContext = cachedIsSecureContext;
+    });
+    it('resolves to `undefined` without throwing', async () => {
+        expect.assertions(1);
+        await expect(assertKeyExporterIsAvailable()).resolves.toBeUndefined();
+    });
+    if (__BROWSER__) {
+        describe('when in an insecure browser context', () => {
+            beforeEach(() => {
+                globalThis.isSecureContext = false;
+            });
+            it('rejects', async () => {
+                expect.assertions(1);
+                await expect(() => assertKeyExporterIsAvailable()).rejects.toThrow();
+            });
+        });
+    }
+    describe('when `SubtleCrypto::exportKey` is not available', () => {
+        let oldExportKey: InstanceType<typeof SubtleCrypto>['exportKey'];
+        beforeEach(() => {
+            oldExportKey = globalThis.crypto.subtle.exportKey;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            globalThis.crypto.subtle.exportKey = undefined;
+        });
+        afterEach(() => {
+            globalThis.crypto.subtle.exportKey = oldExportKey;
+        });
+        it('rejects', async () => {
+            expect.assertions(1);
+            await expect(assertKeyExporterIsAvailable()).rejects.toThrow();
+        });
+    });
+});
+
+describe('assertKeyGenerationIsAvailable()', () => {
+    let assertKeyGenerationIsAvailable: typeof import('../guard').assertKeyGenerationIsAvailable;
+    let cachedIsSecureContext: boolean;
+    beforeEach(async () => {
+        cachedIsSecureContext = globalThis.isSecureContext;
+        globalThis.isSecureContext = true;
+        await jest.isolateModulesAsync(async () => {
+            const guardModulePromise =
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                import('../guard');
+            assertKeyGenerationIsAvailable = (await guardModulePromise).assertKeyGenerationIsAvailable;
+        });
+    });
+    afterEach(() => {
+        globalThis.isSecureContext = cachedIsSecureContext;
+    });
+    it('resolves to `undefined` without throwing', async () => {
+        expect.assertions(1);
+        await expect(assertKeyGenerationIsAvailable()).resolves.toBeUndefined();
+    });
+    if (__BROWSER__) {
+        describe('when in an insecure browser context', () => {
+            beforeEach(() => {
+                globalThis.isSecureContext = false;
+            });
+            it('rejects', async () => {
+                expect.assertions(1);
+                await expect(() => assertKeyGenerationIsAvailable()).rejects.toThrow();
+            });
+        });
+    }
+    describe('when `SubtleCrypto::generateKey` is not available', () => {
+        let oldGenerateKey: InstanceType<typeof SubtleCrypto>['generateKey'];
+        beforeEach(() => {
+            oldGenerateKey = globalThis.crypto.subtle.generateKey;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            globalThis.crypto.subtle.generateKey = undefined;
+        });
+        afterEach(() => {
+            globalThis.crypto.subtle.generateKey = oldGenerateKey;
+        });
+        it('rejects', async () => {
+            expect.assertions(1);
+            await expect(assertKeyGenerationIsAvailable()).rejects.toThrow();
+        });
+    });
+    describe('when the Ed25519 curve is not available', () => {
+        beforeEach(() => {
+            const oldGenerateKey = globalThis.crypto.subtle.generateKey;
+            jest.spyOn(globalThis.crypto.subtle, 'generateKey').mockImplementation(async (algorithm, ...rest) => {
+                if (algorithm === 'Ed25519') {
+                    throw new Error('Ed25519 not supported');
+                }
+                return await oldGenerateKey.call(globalThis.crypto.subtle, algorithm, ...rest);
+            });
+        });
+        it('rejects', async () => {
+            expect.assertions(1);
+            await expect(assertKeyGenerationIsAvailable()).rejects.toThrow();
+        });
+        it('remembers the result from the first time it is called (parallel checks)', async () => {
+            expect.assertions(1);
+            try {
+                await Promise.all([assertKeyGenerationIsAvailable(), assertKeyGenerationIsAvailable()]);
+            } catch {
+                /* empty */
+            }
+            expect(globalThis.crypto.subtle.generateKey).toHaveBeenCalledTimes(1);
+        });
+        it('remembers the result from the first time it is called (serial checks)', async () => {
+            expect.assertions(1);
+            try {
+                await assertKeyGenerationIsAvailable();
+                await assertKeyGenerationIsAvailable();
+            } catch {
+                /* empty */
+            }
+            expect(globalThis.crypto.subtle.generateKey).toHaveBeenCalledTimes(1);
+        });
+    });
+});
+
+describe('assertSigningCapabilityIsAvailable()', () => {
+    let cachedIsSecureContext: boolean;
+    beforeEach(async () => {
+        cachedIsSecureContext = globalThis.isSecureContext;
+        globalThis.isSecureContext = true;
+    });
+    afterEach(() => {
+        globalThis.isSecureContext = cachedIsSecureContext;
+    });
+    it('resolves to `undefined` without throwing', async () => {
+        expect.assertions(1);
+        await expect(assertSigningCapabilityIsAvailable()).resolves.toBeUndefined();
+    });
+    if (__BROWSER__) {
+        describe('when in an insecure browser context', () => {
+            beforeEach(() => {
+                globalThis.isSecureContext = false;
+            });
+            it('rejects', async () => {
+                expect.assertions(1);
+                await expect(() => assertSigningCapabilityIsAvailable()).rejects.toThrow();
+            });
+        });
+    }
+    describe('when `SubtleCrypto::sign` is not available', () => {
+        let oldSign: InstanceType<typeof SubtleCrypto>['sign'];
+        beforeEach(() => {
+            oldSign = globalThis.crypto.subtle.sign;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            globalThis.crypto.subtle.sign = undefined;
+        });
+        afterEach(() => {
+            globalThis.crypto.subtle.sign = oldSign;
+        });
+        it('rejects', async () => {
+            expect.assertions(1);
+            await expect(assertSigningCapabilityIsAvailable()).rejects.toThrow();
+        });
+    });
+});
+
+describe('assertVerificationCapabilityIsAvailable()', () => {
+    let cachedIsSecureContext: boolean;
+    beforeEach(async () => {
+        cachedIsSecureContext = globalThis.isSecureContext;
+        globalThis.isSecureContext = true;
+    });
+    afterEach(() => {
+        globalThis.isSecureContext = cachedIsSecureContext;
+    });
+    it('resolves to `undefined` without throwing', async () => {
+        expect.assertions(1);
+        await expect(assertVerificationCapabilityIsAvailable()).resolves.toBeUndefined();
+    });
+    if (__BROWSER__) {
+        describe('when in an insecure browser context', () => {
+            beforeEach(() => {
+                globalThis.isSecureContext = false;
+            });
+            it('rejects', async () => {
+                expect.assertions(1);
+                await expect(() => assertVerificationCapabilityIsAvailable()).rejects.toThrow();
+            });
+        });
+    }
+    describe('when `SubtleCrypto::sign` is not available', () => {
+        let oldVerify: InstanceType<typeof SubtleCrypto>['verify'];
+        beforeEach(() => {
+            oldVerify = globalThis.crypto.subtle.verify;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            globalThis.crypto.subtle.verify = undefined;
+        });
+        afterEach(() => {
+            globalThis.crypto.subtle.verify = oldVerify;
+        });
+        it('rejects', async () => {
+            expect.assertions(1);
+            await expect(assertVerificationCapabilityIsAvailable()).rejects.toThrow();
+        });
+    });
+});
