6b66b7d4591dc1f621e3922f02590b10fe71dc4b
Callum McIntyre <callum.mcintyre@solana.com>
2023-07-18 15:38:51 +0100

    refactor(experimental): add empty optional config object to RPC methods with no params (#1401)
    This fixes an issue where TS incorrectly types their returns as `unknown`
    
    See https://github.com/solana-labs/solana-web3.js/issues/1389

--- packages/rpc-core/src/rpc-methods/getFirstAvailableBlock.ts
+++ packages/rpc-core/src/rpc-methods/getFirstAvailableBlock.ts
@@ -5,6 +5,10 @@ type GetFirstAvailableBlockApiResponse = Slot;
 export interface GetFirstAvailableBlockApi {
     /**
      * Returns the slot of the lowest confirmed block that has not been purged from the ledger
+     * Note that the optional NO_CONFIG object is ignored. See https://github.com/solana-labs/solana-web3.js/issues/1389
      */
-    getFirstAvailableBlock(): GetFirstAvailableBlockApiResponse;
+    getFirstAvailableBlock(
+        // FIXME: https://github.com/solana-labs/solana-web3.js/issues/1389
+        NO_CONFIG?: Record<string, never>
+    ): GetFirstAvailableBlockApiResponse;
 }
