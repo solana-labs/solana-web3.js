21ecbd9aeeb55905aadb72cb8c07e3ad5a597804
Steven Luscher <steven.luscher@solana.com>
2023-07-19 23:13:59 +0000

    refactor(experimental): a transaction serializer
    ## Summary
    
    This is the final serializer. Start with a transaction, and end up with a transaction in wire format. These are the bytes that you can land on the network.
    
    ## Test Plan
    
    ```
    cd packages/transactions/
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

---
+++ packages/transactions/src/serializers/__tests__/transaction-test.ts
@@ -0,0 +1,89 @@
+import { Base58EncodedAddress } from '@solana/keys';
+
+import { CompiledMessage, compileMessage } from '../../message';
+import { getCompiledMessageEncoder } from '../message';
+import { getTransactionEncoder } from '../transaction';
+
+jest.mock('../../message');
+jest.mock('../message');
+
+let _nextMockAddress = 0;
+function getMockAddress() {
+    return `${_nextMockAddress++}` as Base58EncodedAddress;
+}
+
+describe('Transaction serializer', () => {
+    let addressA: Base58EncodedAddress;
+    let addressB: Base58EncodedAddress;
+    let mockCompiledMessage: CompiledMessage;
+    let mockCompiledWireMessage: Uint8Array;
+    let transaction: ReturnType<typeof getTransactionEncoder>;
+    beforeEach(() => {
+        addressA = getMockAddress();
+        addressB = getMockAddress();
+        mockCompiledMessage = {
+            header: {
+                numReadonlyNonSignerAccounts: 0,
+                numReadonlySignerAccounts: 1,
+                numSignerAccounts: 2,
+            },
+            staticAccounts: [addressB, addressA],
+        } as CompiledMessage;
+        mockCompiledWireMessage = new Uint8Array([1, 2, 3]);
+        (getCompiledMessageEncoder as jest.Mock).mockReturnValue({
+            serialize: jest.fn().mockReturnValue(mockCompiledWireMessage),
+        });
+        (compileMessage as jest.Mock).mockReturnValue(mockCompiledMessage);
+        transaction = getTransactionEncoder();
+    });
+    it('serializes a transaction with no signatures', () => {
+        expect(transaction.serialize({} as Parameters<typeof transaction.serialize>[0])).toStrictEqual(
+            new Uint8Array([
+                /** SIGNATURES */
+                2, // Length of signatures array
+                ...Array(64).fill(0), // null signature for `addressB`
+                ...Array(64).fill(0), // null signature for `addressA`
+
+                /** COMPILED MESSAGE */
+                ...mockCompiledWireMessage,
+            ])
+        );
+    });
+    it('serializes a partially signed transaction', () => {
+        const mockSignatureA = new Uint8Array(Array(64).fill(1));
+        expect(
+            transaction.serialize({ signatures: { [addressA]: mockSignatureA } } as Parameters<
+                typeof transaction.serialize
+            >[0])
+        ).toStrictEqual(
+            new Uint8Array([
+                /** SIGNATURES */
+                2, // Length of signatures array
+                ...Array(64).fill(0), // null signature for `addressB`
+                ...mockSignatureA,
+
+                /** COMPILED MESSAGE */
+                ...mockCompiledWireMessage,
+            ])
+        );
+    });
+    it('serializes a fully signed transaction', () => {
+        const mockSignatureA = new Uint8Array(Array(64).fill(1));
+        const mockSignatureB = new Uint8Array(Array(64).fill(2));
+        expect(
+            transaction.serialize({
+                signatures: { [addressA]: mockSignatureA, [addressB]: mockSignatureB },
+            } as Parameters<typeof transaction.serialize>[0])
+        ).toStrictEqual(
+            new Uint8Array([
+                /** SIGNATURES */
+                2, // Length of signatures array
+                ...mockSignatureB,
+                ...mockSignatureA,
+
+                /** COMPILED MESSAGE */
+                ...mockCompiledWireMessage,
+            ])
+        );
+    });
+});
