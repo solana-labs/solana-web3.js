a6773c29855962d177b54842b438b4f3f880175d
Steven Luscher <steven.luscher@solana.com>
2023-07-19 23:13:59 +0000

    refactor(experimental): a transaction serializer
    ## Summary
    
    This is the final serializer. Start with a transaction, and end up with a transaction in wire format. These are the bytes that you can land on the network.
    
    ## Test Plan
    
    ```
    cd packages/transactions/
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

---
+++ packages/transactions/src/serializers/transaction.ts
@@ -0,0 +1,35 @@
+import { array, bytes, Serializer, shortU16, struct } from '@metaplex-foundation/umi-serializers';
+
+import { getCompiledTransaction } from '../compile-transaction';
+import { getCompiledMessageEncoder } from './message';
+import { getUnimplementedDecoder } from './unimplemented';
+
+const BASE_CONFIG = {
+    description: __DEV__ ? 'The wire format of a Solana transaction' : '',
+    fixedSize: null,
+    maxSize: null,
+} as const;
+
+type SerializableTransaction = Parameters<typeof getCompiledTransaction>[0];
+
+function serialize(transaction: SerializableTransaction) {
+    const compiledTransaction = getCompiledTransaction(transaction);
+    return struct([
+        [
+            'signatures',
+            array(bytes({ size: 64 }), {
+                ...(__DEV__ ? { description: 'A compact array of 64-byte, base-64 encoded Ed25519 signatures' } : null),
+                size: shortU16(),
+            }),
+        ],
+        ['compiledMessage', getCompiledMessageEncoder()],
+    ]).serialize(compiledTransaction);
+}
+
+export function getTransactionEncoder(): Serializer<SerializableTransaction> {
+    return {
+        ...BASE_CONFIG,
+        deserialize: getUnimplementedDecoder('CompiledMessage'),
+        serialize,
+    };
+}
