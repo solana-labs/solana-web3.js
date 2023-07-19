473dec32fa40c97c57098a8924beec3455a1b09a
Steven Luscher <steven.luscher@solana.com>
2023-07-19 21:18:15 +0000

    refactor(experimental): a function you can use to sign a transaction
    ## Summary
    
    Given a `CryptoKeyPair` this function will serialize a transaction to a byte array, sign those bytes, and add the signature to the signatures array at the correct index.
    
    ## Test Plan
    
    ```
    cd packages/transactions/
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

--- packages/transactions/src/signatures.ts
+++ packages/transactions/src/signatures.ts
@@ -1,11 +1,38 @@
-import { Base58EncodedAddress } from '@solana/keys';
+import { Base58EncodedAddress, Ed25519Signature, getBase58EncodedAddressFromPublicKey, signBytes } from '@solana/keys';
 
-type Base64EncodedSignature = string & {
-    readonly __base64EncodedSignature: unique symbol;
-};
+import { CompiledMessage, compileMessage } from './message';
+import { getCompiledMessageEncoder } from './serializers/message';
+
 export interface IFullySignedTransaction extends ITransactionWithSignatures {
     readonly __fullySignedTransaction: unique symbol;
 }
 export interface ITransactionWithSignatures {
-    readonly signatures: Readonly<Record<Base58EncodedAddress, Base64EncodedSignature>>;
+    readonly signatures: { readonly [publicKey: Base58EncodedAddress]: Ed25519Signature };
+}
+
+async function getCompiledMessageSignature(message: CompiledMessage, secretKey: CryptoKey) {
+    const wireMessageBytes = getCompiledMessageEncoder().serialize(message);
+    const signature = await signBytes(secretKey, wireMessageBytes);
+    return signature;
+}
+
+export async function signTransaction<TTransaction extends Parameters<typeof compileMessage>[0]>(
+    keyPair: CryptoKeyPair,
+    transaction: TTransaction | (TTransaction & ITransactionWithSignatures)
+): Promise<TTransaction & ITransactionWithSignatures> {
+    const compiledMessage = compileMessage(transaction);
+    const [signerPublicKey, signature] = await Promise.all([
+        getBase58EncodedAddressFromPublicKey(keyPair.publicKey),
+        getCompiledMessageSignature(compiledMessage, keyPair.privateKey),
+    ]);
+    const nextSignatures = {
+        ...('signatures' in transaction ? transaction.signatures : null),
+        ...({ [signerPublicKey]: signature } as ITransactionWithSignatures['signatures']),
+    };
+    const out = {
+        ...transaction,
+        signatures: nextSignatures,
+    };
+    Object.freeze(out);
+    return out;
 }
