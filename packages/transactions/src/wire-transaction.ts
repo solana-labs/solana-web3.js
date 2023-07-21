20be8d5ddb1c53829864717596ae5a130964476a
Steven Luscher <steven.luscher@solana.com>
2023-07-21 05:29:21 +0000

    refactor(experimental): a method to serialize a transaction to wire format
    ## Summary
    
    This function gives you the thing that you will use with `sendTransaction()` to land a transaction on the network.
    
    ## Test Plan
    
    ```
    cd packages/transactions/
    pnpm test:unit:browser
    pnpm test:unit:node
    ```

---
+++ packages/transactions/src/wire-transaction.ts
@@ -0,0 +1,16 @@
+import { getTransactionEncoder } from './serializers/transaction';
+
+export type Base64EncodedWireTransaction = string & {
+    readonly __base64EncodedWireTransaction: unique symbol;
+};
+
+export function getBase64EncodedWireTransaction(
+    transaction: Parameters<ReturnType<typeof getTransactionEncoder>['serialize']>[0]
+): Base64EncodedWireTransaction {
+    const wireTransactionBytes = getTransactionEncoder().serialize(transaction);
+    if (__NODEJS__) {
+        return Buffer.from(wireTransactionBytes).toString('base64') as Base64EncodedWireTransaction;
+    } else {
+        return (btoa as Window['btoa'])(String.fromCharCode(...wireTransactionBytes)) as Base64EncodedWireTransaction;
+    }
+}
