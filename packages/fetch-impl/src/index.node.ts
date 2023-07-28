91405cb2d1e84e5b48ea6e0fee8ee663aeef9cf3
Steven Luscher <steveluscher@users.noreply.github.com>
2023-07-28 16:27:03 -0700

    fix: use the native `fetch()` in Node wherever supported (#1447)
    # Summary
    
    Folks are having trouble using the `node-fetch` polyfill with certain RPC providers that:
    
    * use chunked-transfer encoding
    * don't send a zero-sized chunk before closing the socket
    
    `node-fetch` doesn't like this, and fatals.
    
    One easy thing we can do while we figure this all out is to use the _native_ `fetch()` API included in Node 18+ by default and 17.5+ behind an experimental flag.
    
    Fixes #1418 in Node 18+.
    
    # Test Plan
    
    CI run.

--- packages/fetch-impl/src/index.node.ts
+++ packages/fetch-impl/src/index.node.ts
@@ -1,4 +1,8 @@
 // When building the browser bundle, this import gets replaced by `globalThis.fetch`.
 import fetchImpl from 'node-fetch';
 
-export default fetchImpl;
+export default typeof globalThis.fetch === 'function'
+    ? // The Fetch API is supported experimentally in Node 17.5+ and natively in Node 18+.
+      globalThis.fetch
+    : // Otherwise use the polyfill.
+      fetchImpl;
