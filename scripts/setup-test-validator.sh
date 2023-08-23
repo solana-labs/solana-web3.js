41834796ba4b6129a6e6db1d5fb2f1466522610f
Steven Luscher <steven.luscher@solana.com>
2023-08-23 04:34:55 +0000

    chore: setup now installs the same version of the validator that runs in CI

--- scripts/setup-test-validator.sh
+++ scripts/setup-test-validator.sh
@@ -3,6 +3,9 @@
 set -ex
 
 # setup environment
-sh -c "$(curl -sSfL https://release.solana.com/edge/install)"
+version=$(curl -s -N https://api.github.com/repos/solana-labs/solana/releases/latest \
+    | grep -m 1 \"tag_name\" \
+    | sed -ne 's/^ *"tag_name": "\([^"]*\)",$/\1/p')
+sh -c "$(curl -sSfL https://release.solana.com/$version/install)"
 PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
 solana --version
