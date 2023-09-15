3c38083dab4161f214608ac8b8c5455c6351310e
Steven Luscher <steveluscher@users.noreply.github.com>
2023-09-15 12:02:40 -0700

    refactor(experimental): install the autopinger by default (#1603)
    # Summary
    
    Creating a default subscriptions transport now comes with the auto-pinger by default.
    
    # Test Plan
    
    Load up the browser bundle and run this test case.
    
    ```ts
    const rpc = solanaWeb3.createSolanaRpcSubscriptions({transport: solanaWeb3.createDefaultRpcSubscriptionsTransport({url :'wss://api.devnet.solana.com'})});
    const notifications = await rpc.accountNotifications('CXPeim1wQMkcTvEHx9QdhgKREYYJD8bnaCCqPRwJ1to1').subscribe();
    for await (const notif of notifications) {
      console.log(notif);
    }
    ```
    
    Observe pings every 5 seconds.

--- packages/library/src/rpc-websocket-transport.ts
+++ packages/library/src/rpc-websocket-transport.ts
@@ -1,16 +1,23 @@
 import { createWebSocketTransport } from '@solana/rpc-transport';
 import { IRpcWebSocketTransport } from '@solana/rpc-transport/dist/types/transports/transport-types';
 
+import { getWebSocketTransportWithAutoping } from './rpc-websocket-autopinger';
+
 export function createDefaultRpcSubscriptionsTransport(
     config: Omit<Parameters<typeof createWebSocketTransport>[0], 'sendBufferHighWatermark'> & {
+        intervalMs?: number;
         sendBufferHighWatermark?: number;
     }
 ): IRpcWebSocketTransport {
-    return createWebSocketTransport({
-        ...config,
-        sendBufferHighWatermark:
-            config.sendBufferHighWatermark ??
-            // Let 128KB of data into the WebSocket buffer before buffering it in the app.
-            131_072,
+    const { intervalMs, ...rest } = config;
+    return getWebSocketTransportWithAutoping({
+        intervalMs: intervalMs ?? 5_000,
+        transport: createWebSocketTransport({
+            ...rest,
+            sendBufferHighWatermark:
+                config.sendBufferHighWatermark ??
+                // Let 128KB of data into the WebSocket buffer before buffering it in the app.
+                131_072,
+        }),
     });
 }
