e9606c38414f00d4b954db7a21a49ba592a3fa4d
Loris Leiva <loris.leiva@gmail.com>
2023-09-18 16:07:19 +0100

    feat(experimental): add mapEncoder and mapDecoder functions

--- packages/codecs-core/src/mapCodec.ts
+++ packages/codecs-core/src/mapCodec.ts
@@ -1,5 +1,35 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
-import { Codec } from './codec';
+import { Codec, Decoder, Encoder } from './codec';
+
+/**
+ * Converts an encoder A to a encoder B by mapping their values.
+ */
+export function mapEncoder<T, U>(encoder: Encoder<T>, unmap: (value: U) => T): Encoder<U> {
+    return {
+        description: encoder.description,
+        fixedSize: encoder.fixedSize,
+        maxSize: encoder.maxSize,
+        encode: (value: U) => encoder.encode(unmap(value)),
+    };
+}
+
+/**
+ * Converts an decoder A to a decoder B by mapping their values.
+ */
+export function mapDecoder<T, U>(
+    decoder: Decoder<T>,
+    map: (value: T, buffer: Uint8Array, offset: number) => U
+): Decoder<U> {
+    return {
+        description: decoder.description,
+        fixedSize: decoder.fixedSize,
+        maxSize: decoder.maxSize,
+        decode: (buffer: Uint8Array, offset = 0) => {
+            const [value, length] = decoder.decode(buffer, offset);
+            return [map(value, buffer, offset), length];
+        },
+    };
+}
 
 /**
  * Converts a codec A to a codec B by mapping their values.
@@ -22,10 +52,7 @@ export function mapCodec<NewFrom, OldFrom, NewTo extends NewFrom = NewFrom, OldT
         description: codec.description,
         fixedSize: codec.fixedSize,
         maxSize: codec.maxSize,
-        encode: (value: NewFrom) => codec.encode(unmap(value)),
-        decode: (buffer: Uint8Array, offset = 0) => {
-            const [value, length] = codec.decode(buffer, offset);
-            return map ? [map(value, buffer, offset), length] : [value as unknown as NewTo, length];
-        },
+        encode: mapEncoder(codec, unmap).encode,
+        decode: map ? mapDecoder(codec, map).decode : (codec.decode as unknown as Decoder<NewTo>['decode']),
     };
 }
