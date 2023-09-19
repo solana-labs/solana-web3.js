3739d33d4026d036391065882303a8be84934cbd
Loris Leiva <loris.leiva@gmail.com>
2023-09-19 17:37:27 +0100

    feat(experimental): add NumberEncoder and NumberDecoder types

--- packages/codecs-numbers/src/common.ts
+++ packages/codecs-numbers/src/common.ts
@@ -1,18 +1,18 @@
-import { BaseCodecOptions, Codec } from '@solana/codecs-core';
+import { BaseCodecOptions, Codec, Decoder, Encoder } from '@solana/codecs-core';
 
-/**
- * Defines a serializer for numbers and bigints.
- */
+/** Defines a encoder for numbers and bigints. */
+export type NumberEncoder = Encoder<number> | Encoder<number | bigint>;
+
+/** Defines a decoder for numbers and bigints. */
+export type NumberDecoder = Decoder<number> | Decoder<bigint>;
+
+/** Defines a codec for numbers and bigints. */
 export type NumberCodec = Codec<number> | Codec<number | bigint, bigint>;
 
-/**
- * Defines the options for u8 and i8 serializers.
- */
+/** Defines the options for u8 and i8 codecs. */
 export type SingleByteNumberCodecOptions = BaseCodecOptions;
 
-/**
- * Defines the options for number serializers that use more than one byte.
- */
+/** Defines the options for number codecs that use more than one byte. */
 export type NumberCodecOptions = BaseCodecOptions & {
     /**
      * Whether the serializer should use little-endian or big-endian encoding.
@@ -21,9 +21,7 @@ export type NumberCodecOptions = BaseCodecOptions & {
     endian?: Endian;
 };
 
-/**
- * Defines the endianness of a number serializer.
- */
+/** Defines the endianness of a number serializer. */
 export enum Endian {
     Little = 'le',
     Big = 'be',
