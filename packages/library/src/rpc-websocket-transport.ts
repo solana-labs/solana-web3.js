f488443b155d54072ee122e609884b373184ced9
Steven Luscher <steven.luscher@solana.com>
2023-09-19 23:53:24 +0000

    refactor(experimental): install the sharding transport by default in web3.js
    # Summary
    
    This meta-transport makes all subscriptions share a single socket connection, unless the user supplies a `getShard` method to produce unique shard keys from payloads.
    
    # Test Plan
    
    This in `about:blank`: https://gist.github.com/steveluscher/ea01a212d50c88ee53a712c0cc2c976a

--- packages/library/src/rpc-websocket-transport.ts
+++ packages/library/src/rpc-websocket-transport.ts
@@ -2,22 +2,32 @@ import { createWebSocketTransport } from '@solana/rpc-transport';
 import { IRpcWebSocketTransport } from '@solana/rpc-transport/dist/types/transports/transport-types';
 
 import { getWebSocketTransportWithAutoping } from './rpc-websocket-autopinger';
+import { getWebSocketTransportWithConnectionSharding } from './rpc-websocket-connection-sharding';
 
 export function createDefaultRpcSubscriptionsTransport(
     config: Omit<Parameters<typeof createWebSocketTransport>[0], 'sendBufferHighWatermark'> & {
+        /**
+         * You might like to open more subscriptions per connection than your RPC provider allows
+         * for. Using the initial payload as input, return a shard key from this method to assign
+         * subscriptions to separate connections. One socket will be opened per shard key.
+         */
+        getShard?: (payload: unknown) => string;
         intervalMs?: number;
         sendBufferHighWatermark?: number;
     }
 ): IRpcWebSocketTransport {
-    const { intervalMs, ...rest } = config;
+    const { getShard, intervalMs, ...rest } = config;
     return getWebSocketTransportWithAutoping({
         intervalMs: intervalMs ?? 5_000,
-        transport: createWebSocketTransport({
-            ...rest,
-            sendBufferHighWatermark:
-                config.sendBufferHighWatermark ??
-                // Let 128KB of data into the WebSocket buffer before buffering it in the app.
-                131_072,
+        transport: getWebSocketTransportWithConnectionSharding({
+            getShard,
+            transport: createWebSocketTransport({
+                ...rest,
+                sendBufferHighWatermark:
+                    config.sendBufferHighWatermark ??
+                    // Let 128KB of data into the WebSocket buffer before buffering it in the app.
+                    131_072,
+            }),
         }),
     });
 }
