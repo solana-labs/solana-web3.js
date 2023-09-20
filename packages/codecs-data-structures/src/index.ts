02e04d7e993167eb0c741b3b20eac4f191468d29
Loris Leiva <loris.leiva@gmail.com>
2023-09-20 16:12:29 +0100

    feat(experimental): add nullable codec

--- packages/codecs-data-structures/src/index.ts
+++ packages/codecs-data-structures/src/index.ts
@@ -3,5 +3,6 @@ export * from './array-like-codec-size';
 export * from './boolean';
 export * from './errors';
 export * from './map';
+export * from './nullable';
 export * from './set';
 export * from './unit';
