55e373b2e48becf999a67ce4cb236f8265987a60
Joe C <joecaulfield29@yahoo.com>
2023-09-21 02:08:24 +0200

    refactor(experimental): Add `@solana/functional` (#1576)
    * refactor(experimental): add solana functional lib
    
    * refactor(experimental): revised pipe
    
    * refactor(experimental): extend pipe arity to 10
    
    * refactor(experimental): update functional tests and dependencies
    
    * refactor(experimental): update functional README
    
    * refactor(experimental): added tests to functional mocking tx building
    
    * refactor(experimental): add nested pipe tests to functional
    
    * refactor(experimental): add functional copy-clone tests
    
    * refactor(experimental): remove expect assertions in functional tests

---
+++ packages/functional/README.md
@@ -0,0 +1,24 @@
+[![npm][npm-image]][npm-url]
+[![npm-downloads][npm-downloads-image]][npm-url]
+[![semantic-release][semantic-release-image]][semantic-release-url]
+<br />
+[![code-style-prettier][code-style-prettier-image]][code-style-prettier-url]
+
+[code-style-prettier-image]: https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square
+[code-style-prettier-url]: https://github.com/prettier/prettier
+[npm-downloads-image]: https://img.shields.io/npm/dm/@solana/functional/experimental.svg?style=flat
+[npm-image]: https://img.shields.io/npm/v/@solana/functional/experimental.svg?style=flat
+[npm-url]: https://www.npmjs.com/package/@solana/functional/v/experimental
+[semantic-release-image]: https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg
+[semantic-release-url]: https://github.com/semantic-release/semantic-release
+
+# @solana/functional
+
+This package contains generalized functional helpers and functional helpers
+specific to Solana application components. It can be used standalone, but it
+is also exported as part of the Solana JavaScript SDK
+[`@solana/web3.js@experimental`](https://github.com/solana-labs/solana-web3.js/tree/master/packages/library).
+
+## Functions
+
+`pipe`: A functional utility function for piping a value through various functions
---
+++ packages/functional/src/pipe.ts
@@ -0,0 +1,108 @@
+/**
+ * General pipe function.
+ * Provide an initial value and a list of functions to pipe it through.
+ *
+ * Following common implementations of pipe functions that use TypeScript,
+ * this function supports a maximum arity of 10 for type safety.
+ * @see https://github.com/ramda/ramda/blob/master/source/pipe.js
+ * @see https://github.com/darky/rocket-pipes/blob/master/index.ts
+ *
+ * Note you can use nested pipes to extend this limitation, like so:
+ * ```typescript
+ * const myValue = pipe(
+ *      pipe(
+ *          1,
+ *          (x) => x + 1,
+ *          (x) => x * 2,
+ *          (x) => x - 1,
+ *      ),
+ *      (y) => y / 3,
+ *      (y) => y + 1,
+ * );
+ * ```
+ * @param init  The initial value
+ * @param fns   Any number of functions to pipe the value through
+ * @returns     The final value with all functions applied
+ */
+export function pipe<TInitial>(init: TInitial): TInitial;
+export function pipe<TInitial, R1>(init: TInitial, init_r1: (init: TInitial) => R1): R1;
+export function pipe<TInitial, R1, R2>(init: TInitial, init_r1: (init: TInitial) => R1, r1_r2: (r1: R1) => R2): R2;
+export function pipe<TInitial, R1, R2, R3>(
+    init: TInitial,
+    init_r1: (init: TInitial) => R1,
+    r1_r2: (r1: R1) => R2,
+    r2_r3: (r2: R2) => R3
+): R3;
+export function pipe<TInitial, R1, R2, R3, R4>(
+    init: TInitial,
+    init_r1: (init: TInitial) => R1,
+    r1_r2: (r1: R1) => R2,
+    r2_r3: (r2: R2) => R3,
+    r3_r4: (r3: R3) => R4
+): R4;
+export function pipe<TInitial, R1, R2, R3, R4, R5>(
+    init: TInitial,
+    init_r1: (init: TInitial) => R1,
+    r1_r2: (r1: R1) => R2,
+    r2_r3: (r2: R2) => R3,
+    r3_r4: (r3: R3) => R4,
+    r4_r5: (r4: R4) => R5
+): R5;
+export function pipe<TInitial, R1, R2, R3, R4, R5, R6>(
+    init: TInitial,
+    init_r1: (init: TInitial) => R1,
+    r1_r2: (r1: R1) => R2,
+    r2_r3: (r2: R2) => R3,
+    r3_r4: (r3: R3) => R4,
+    r4_r5: (r4: R4) => R5,
+    r5_r6: (r5: R5) => R6
+): R6;
+export function pipe<TInitial, R1, R2, R3, R4, R5, R6, R7>(
+    init: TInitial,
+    init_r1: (init: TInitial) => R1,
+    r1_r2: (r1: R1) => R2,
+    r2_r3: (r2: R2) => R3,
+    r3_r4: (r3: R3) => R4,
+    r4_r5: (r4: R4) => R5,
+    r5_r6: (r5: R5) => R6,
+    r6_r7: (r6: R6) => R7
+): R7;
+export function pipe<TInitial, R1, R2, R3, R4, R5, R6, R7, R8>(
+    init: TInitial,
+    init_r1: (init: TInitial) => R1,
+    r1_r2: (r1: R1) => R2,
+    r2_r3: (r2: R2) => R3,
+    r3_r4: (r3: R3) => R4,
+    r4_r5: (r4: R4) => R5,
+    r5_r6: (r5: R5) => R6,
+    r6_r7: (r6: R6) => R7,
+    r7_r8: (r7: R7) => R8
+): R8;
+export function pipe<TInitial, R1, R2, R3, R4, R5, R6, R7, R8, R9>(
+    init: TInitial,
+    init_r1: (init: TInitial) => R1,
+    r1_r2: (r1: R1) => R2,
+    r2_r3: (r2: R2) => R3,
+    r3_r4: (r3: R3) => R4,
+    r4_r5: (r4: R4) => R5,
+    r5_r6: (r5: R5) => R6,
+    r6_r7: (r6: R6) => R7,
+    r7_r8: (r7: R7) => R8,
+    r8_r9: (r8: R8) => R9
+): R9;
+export function pipe<TInitial, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10>(
+    init: TInitial,
+    init_r1: (init: TInitial) => R1,
+    r1_r2: (r1: R1) => R2,
+    r2_r3: (r2: R2) => R3,
+    r3_r4: (r3: R3) => R4,
+    r4_r5: (r4: R4) => R5,
+    r5_r6: (r5: R5) => R6,
+    r6_r7: (r6: R6) => R7,
+    r7_r8: (r7: R7) => R8,
+    r8_r9: (r8: R8) => R9,
+    r9_r10: (r9: R9) => R10
+): R10;
+export function pipe<TInitial>(init: TInitial, ...fns: CallableFunction[]) {
+    return fns.reduce((acc, fn) => fn(acc), init);
+}
