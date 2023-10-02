3332e96e1fe7e6f6730b6e0d8d5f9630dd01cb23
Joe <jcaulfield135@gmail.com>
2023-10-02 19:53:32 +0200

    refactor(experimental): a bigint scalar for RPC GraphQL

---
+++ packages/rpc-graphql/src/schema/scalars.ts
@@ -0,0 +1,20 @@
+import { GraphQLScalarType, Kind } from 'graphql';
+
+export const BigIntScalar = new GraphQLScalarType({
+    name: 'BigInt',
+
+    parseLiteral(ast): bigint | null {
+        if (ast.kind === Kind.STRING) {
+            return BigInt(ast.value);
+        }
+        return null;
+    },
+
+    parseValue(value): bigint {
+        return BigInt(value as string);
+    },
+
+    serialize(value): bigint {
+        return BigInt(value as string);
+    },
+});
