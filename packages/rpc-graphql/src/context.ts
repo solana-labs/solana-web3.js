c2370890279f35c0134df105270046056763f30d
Joe C <joecaulfield29@yahoo.com>
2023-10-11 17:22:25 +0200

    refactor(experimental): an account resolver
    A resolver to hit the Solana RPC for an `AccountInfo` from a GraphQL query.

--- packages/rpc-graphql/src/context.ts
+++ packages/rpc-graphql/src/context.ts
@@ -2,13 +2,62 @@ import { SolanaRpcMethods } from '@solana/rpc-core';
 import { Rpc } from '@solana/rpc-transport/dist/types/json-rpc-types';
 
 import { createGraphQLCache, GraphQLCache } from './cache';
+import { AccountQueryArgs } from './schema/account/query';
 
 export interface RpcGraphQLContext {
     cache: GraphQLCache;
+    resolveAccount(args: AccountQueryArgs): ReturnType<typeof resolveAccount>;
     rpc: Rpc<SolanaRpcMethods>;
+}
+
+// Default to jsonParsed encoding if none is provided
+async function resolveAccount(
+    { address, encoding = 'jsonParsed', ...config }: AccountQueryArgs,
+    cache: GraphQLCache,
+    rpc: Rpc<SolanaRpcMethods>
+) {
+    const requestConfig = { encoding, ...config };
+
+    const cached = cache.get(address, requestConfig);
+    if (cached !== null) {
+        return cached;
+    }
+
+    const account = await rpc
+        .getAccountInfo(address, requestConfig as Parameters<SolanaRpcMethods['getAccountInfo']>[1])
+        .send()
+        .then(res => res.value)
+        .catch(e => {
+            throw e;
+        });
+
+    if (account === null) {
+        return null;
+    }
+
+    const [data, responseEncoding] = Array.isArray(account.data)
+        ? encoding === 'jsonParsed'
+            ? [account.data[0], 'base64']
+            : [account.data[0], encoding]
+        : [account.data, 'jsonParsed'];
+    const queryResponse = {
+        ...account,
+        data,
+        encoding: responseEncoding,
+    };
+
+    cache.insert(address, requestConfig, queryResponse);
+
+    return queryResponse;
 }
 
 export function createSolanaGraphQLContext(rpc: Rpc<SolanaRpcMethods>): RpcGraphQLContext {
     const cache = createGraphQLCache();
-    return { cache, rpc };
+    return {
+        cache,
+        resolveAccount(args) {
+            return resolveAccount(args, this.cache, this.rpc);
+        },
+        rpc,
+    };
 }
