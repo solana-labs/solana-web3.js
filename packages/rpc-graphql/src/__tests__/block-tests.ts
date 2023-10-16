ee4214c2388eb444084dee7c7b01e0d6cc6f89ec
Joe C <joecaulfield29@yahoo.com>
2023-10-16 10:52:52 +0200

    refactor(experimental): graphql: remove transaction tests (#1733)

--- packages/rpc-graphql/src/__tests__/block-tests.ts
+++ packages/rpc-graphql/src/__tests__/block-tests.ts
@@ -1,65 +1,6 @@
-/* eslint-disable sort-keys-fix/sort-keys-fix */
-import { createSolanaRpcApi, SolanaRpcMethods } from '@solana/rpc-core';
-import { createHttpTransport, createJsonRpc } from '@solana/rpc-transport';
-import { Rpc } from '@solana/rpc-transport/dist/types/json-rpc-types';
-
-import { createRpcGraphQL, RpcGraphQL } from '../rpc';
-
 describe('block', () => {
-    let rpc: Rpc<SolanaRpcMethods>;
-    let rpcGraphQL: RpcGraphQL;
-    beforeEach(() => {
-        fetchMock.resetMocks();
-        fetchMock.dontMock();
-        rpc = createJsonRpc<SolanaRpcMethods>({
-            api: createSolanaRpcApi(),
-            transport: createHttpTransport({ url: 'http://127.0.0.1:8899' }),
-        });
-        rpcGraphQL = createRpcGraphQL(rpc);
-    });
-
-    // TODO: These are not always going to work (can't guarantee slot 0 exists)
     describe('basic queries', () => {
-        const variableValues = {
-            slot: 0n,
-            commitment: 'confirmed',
-        };
-        it("can query a block's blockhash", async () => {
-            expect.assertions(1);
-            const source = `
-                query testQuery($slot: BigInt!, $commitment: Commitment) {
-                    block(slot: $slot, commitment: $commitment) {
-                        blockhash
-                    }
-                }
-            `;
-            const result = await rpcGraphQL.query(source, variableValues);
-            expect(result).toMatchObject({
-                data: {
-                    block: {
-                        blockhash: expect.any(String),
-                    },
-                },
-            });
-        });
-        it("can query a block's block time", async () => {
-            expect.assertions(1);
-            const source = `
-                query testQuery($slot: BigInt!, $commitment: Commitment) {
-                    block(slot: $slot, commitment: $commitment) {
-                        blockTime
-                    }
-                }
-            `;
-            const result = await rpcGraphQL.query(source, variableValues);
-            expect(result).toMatchObject({
-                data: {
-                    block: {
-                        blockTime: expect.any(BigInt),
-                    },
-                },
-            });
-        });
+        it.todo("can query a block's block time");
     });
     describe('block with signatures transaction details', () => {
         it.todo('can query a block with signatures');
