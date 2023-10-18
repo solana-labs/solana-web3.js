80df00e4fd667aa0ba061d23682af6b997197d03
steveluscher <me+github@steveluscher.com>
2023-10-18 05:30:57 +0000

    refactor(experimental): massage test names in `@solana/compat`

--- packages/compat/src/__tests__/address-test.ts
+++ packages/compat/src/__tests__/address-test.ts
@@ -2,7 +2,7 @@ import { Keypair } from '@solana/web3.js';
 
 import { fromLegacyPublicKey } from '../address';
 
-describe('address', () => {
+describe('fromLegacyPublicKey', () => {
     it('should convert from a Legacy Web3 JS PublicKey to a Base58EncodedAddress', () => {
         const publicKey = Keypair.generate().publicKey;
         expect(publicKey.toBase58()).toEqual(fromLegacyPublicKey(publicKey));
