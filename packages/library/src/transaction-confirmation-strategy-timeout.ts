24b3ddaa39ab97ffc4774437a4c9fb4ea5628dae
steveluscher <me+github@steveluscher.com>
2023-10-19 00:43:11 +0000

    refactor(experimental): a timeout-based transaction confirmation strategy

---
+++ packages/library/src/transaction-confirmation-strategy-timeout.ts
@@ -0,0 +1,27 @@
+import { Commitment } from '@solana/rpc-core';
+
+type Config = Readonly<{
+    abortSignal: AbortSignal;
+    commitment: Commitment;
+}>;
+
+export async function getTimeoutPromise({ abortSignal: callerAbortSignal, commitment }: Config) {
+    return await new Promise((_, reject) => {
+        const handleAbort = (e: AbortSignalEventMap['abort']) => {
+            clearTimeout(timeoutId);
+            const abortError = new DOMException((e.target as AbortSignal).reason, 'AbortError');
+            reject(abortError);
+        };
+        callerAbortSignal.addEventListener('abort', handleAbort);
+        const timeoutMs = commitment === 'processed' ? 30_000 : 60_000;
+        const startMs = performance.now();
+        const timeoutId =
+            // We use `setTimeout` instead of `AbortSignal.timeout()` because we want to measure
+            // elapsed time instead of active time.
+            // See https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal/timeout_static
+            setTimeout(() => {
+                const elapsedMs = performance.now() - startMs;
+                reject(new DOMException(`Timeout elapsed after ${elapsedMs} ms`, 'TimeoutError'));
+            }, timeoutMs);
+    });
+}
