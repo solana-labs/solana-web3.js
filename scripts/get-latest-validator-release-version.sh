be901cfad1b6b42734c99392737ee3d6fa6032e6
steveluscher <me+github@steveluscher.com>
2023-10-21 00:37:52 +0000

    chore: make the test validator version and the CI cache key match

--- scripts/get-latest-validator-release-version.sh
+++ scripts/get-latest-validator-release-version.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 (
     set -e
-    version=$(curl -s "https://api.github.com/repos/solana-labs/solana/releases" | \
-      grep '"tag_name": "v1.17.' | \
-      sort -t. -k3,3nr | \
-      head -n1 | \
-      awk -F\" '{print $4}')
+    version=$(node -e \
+      'fetch("https://api.github.com/repos/solana-labs/solana/releases").then(res => res.json().then(rs => rs.filter(r => !r.prerelease && r.tag_name.startsWith("v1.17."))).then(x => console.log(x[0].tag_name)));'
+    )
     if [ -z $version ]; then
       exit 3
     fi
--- scripts/setup-test-validator.sh
+++ scripts/setup-test-validator.sh
@@ -2,11 +2,10 @@
 
 set -ex
 
-TARGET_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")/.." ; pwd -P )/.solana
+SCRIPTS_DIR=$(dirname "${BASH_SOURCE[0]}")
+TARGET_DIR=$( cd "$SCRIPTS_DIR/.." ; pwd -P )/.solana
 
 # setup environment
-version=$(node -e \
-  'fetch("https://api.github.com/repos/solana-labs/solana/releases").then(res => res.json().then(rs => rs.filter(r => !r.prerelease && r.tag_name.startsWith("v1.17."))).then(x => console.log(x[0].tag_name)));'
-)
+version=$(bash $SCRIPTS_DIR/get-latest-validator-release-version.sh)
 sh -c "$(curl -sSfL https://release.solana.com/$version/install)" init $version --data-dir $TARGET_DIR --no-modify-path
 $TARGET_DIR/active_release/bin/solana --version
