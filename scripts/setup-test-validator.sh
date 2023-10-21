70ec74606c4e03546d42a6213aeaa23fe799100a
steveluscher <me+github@steveluscher.com>
2023-10-21 00:31:53 +0000

    chore: parse the releases JSON filtering out prerelease versions

--- scripts/setup-test-validator.sh
+++ scripts/setup-test-validator.sh
@@ -5,10 +5,8 @@ set -ex
 TARGET_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")/.." ; pwd -P )/.solana
 
 # setup environment
-version=$(curl -s "https://api.github.com/repos/solana-labs/solana/releases" | \
-      grep '"tag_name": "v1.17.' | \
-      sort -t. -k3,3nr | \
-      head -n1 | \
-      awk -F\" '{print $4}')
+version=$(node -e \
+  'fetch("https://api.github.com/repos/solana-labs/solana/releases").then(res => res.json().then(rs => rs.filter(r => !r.prerelease && r.tag_name.startsWith("v1.17."))).then(x => console.log(x[0].tag_name)));'
+)
 sh -c "$(curl -sSfL https://release.solana.com/$version/install)" init $version --data-dir $TARGET_DIR --no-modify-path
 $TARGET_DIR/active_release/bin/solana --version
