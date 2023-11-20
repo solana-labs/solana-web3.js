735654a42b08b610f88d123add6232b1a83d16ec
Steven Luscher <steveluscher@users.noreply.github.com>
2023-11-20 13:54:29 -0800

    refactor(experimental): add constants to web3.js sham (#1862)
    Addresses #1825.

--- packages/library-legacy-sham/src/index.ts
+++ packages/library-legacy-sham/src/index.ts
@@ -1,1 +1,9 @@
 export * from './public-key';
+
+export const LAMPORTS_PER_SOL = 1_000_000_000;
+export const MAX_SEED_LENGTH = 32;
+export const NONCE_ACCOUNT_LENGTH = 80;
+export const PACKET_DATA_SIZE = 1280 - 40 - 8;
+export const PUBLIC_KEY_LENGTH = 32;
+export const SIGNATURE_LENGTH_IN_BYTES = 64;
+export const VERSION_PREFIX_MASK = 0x7f;
