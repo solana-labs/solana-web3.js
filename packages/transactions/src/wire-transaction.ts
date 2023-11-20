433f475e8f9d28f78e5a87df7e500cb5d8643167
Loris Leiva <loris.leiva@gmail.com>
2023-11-20 12:08:12 +0000

    refactor(experimental): use getBase64Decoder in getBase64EncodedWireTransaction (#1857)
    Refactor the implementation of `getBase64EncodedWireTransaction` such that it uses the `getBase64Decoder` instead of re-implementing its logic.

--- packages/transactions/src/wire-transaction.ts
+++ packages/transactions/src/wire-transaction.ts
@@ -1,3 +1,5 @@
+import { getBase64Decoder } from '@solana/codecs-strings';
+
 import { getTransactionEncoder } from './serializers/transaction';
 
 export type Base64EncodedWireTransaction = string & {
@@ -8,9 +10,5 @@ export function getBase64EncodedWireTransaction(
     transaction: Parameters<ReturnType<typeof getTransactionEncoder>['encode']>[0]
 ): Base64EncodedWireTransaction {
     const wireTransactionBytes = getTransactionEncoder().encode(transaction);
-    if (__NODEJS__) {
-        return Buffer.from(wireTransactionBytes).toString('base64') as Base64EncodedWireTransaction;
-    } else {
-        return (btoa as Window['btoa'])(String.fromCharCode(...wireTransactionBytes)) as Base64EncodedWireTransaction;
-    }
+    return getBase64Decoder().decode(wireTransactionBytes)[0] as Base64EncodedWireTransaction;
 }
