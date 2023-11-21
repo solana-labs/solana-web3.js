0921bfe1db85ec2c45ce21302102aad06cbc29fc
steveluscher <me+github@steveluscher.com>
2023-11-21 20:11:01 +0000

    refactor(experimental): create a `Connection` sham that only exposes `rpcEndpoint`

---
+++ packages/library-legacy-sham/src/__tests__/connection-test.ts
@@ -0,0 +1,12 @@
+import { Connection } from '../connection';
+
+describe('ConnectionSham', () => {
+    it.each(['gopher:', 'tel:', 'ws://', 'wss://'])('fatals if the endpoint supplied starts with `%s`', scheme => {
+        expect(() => new Connection(scheme + 'url')).toThrow();
+    });
+    it.each(['https', 'http'])('offers the `%s` endpoint URL through the `rpcEndpoint` property', scheme => {
+        const endpoint = scheme + '://url';
+        const connection = new Connection(endpoint);
+        expect(connection).toHaveProperty('rpcEndpoint', endpoint);
+    });
+});
---
+++ packages/library-legacy-sham/src/__typetests__/connection-typetests.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { Connection as LegacyConnection } from '@solana/web3.js-legacy';
+
+import { Connection } from '../connection';
+
+new Connection('https://some.rpc').rpcEndpoint satisfies 'https://some.rpc';
+
+// @ts-expect-error This is only a partial sham
+new Connection('https://some.rpc') satisfies LegacyConnection;
+
+// @ts-expect-error This is only a partial sham
+Connection satisfies typeof LegacyConnection;
