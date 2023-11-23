2aa2b633411e28ff2bd25adc1dd155aca6c202b7
steveluscher <me+github@steveluscher.com>
2023-11-23 01:05:16 +0000

    refactor(experimental): a sham for `Transaction`

---
+++ packages/library-legacy-sham/src/__typetests__/transaction-typetests.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { Transaction as LegacyTransaction } from '@solana/web3.js-legacy';
+
+import { Transaction } from '../transaction';
+
+new Transaction().signature satisfies Buffer | null;
+
+new Transaction() satisfies LegacyTransaction;
+
+Transaction satisfies typeof LegacyTransaction;
