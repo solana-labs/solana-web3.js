2889ecff534a33acb6063e0741ad2d01f95f1de0
steveluscher <me+github@steveluscher.com>
2023-11-27 19:14:20 +0000

    refactor(experimental): build only packages relevant for platform

--- packages/fetch-impl/tsup.config.ts
+++ packages/fetch-impl/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig(_options =>
     (['browser', 'node'] as const).map(platform => ({
-        entry: [`./src/**`],
+        entry: [`./src/index.${platform}.ts`],
         format: ['cjs', 'esm'],
         minify: true,
         name: platform,
