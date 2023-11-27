dd05157d4cbc68eb6e51ffbdfbe739cc10acbb1d
Callum McIntyre <callum.mcintyre@solana.com>
2023-11-27 11:24:21 +0000

    refactor(experimental): use exported types for slot-notifications-type-test.ts

--- packages/rpc-core/src/rpc-subscriptions/__typetests__/slot-notifications-type-test.ts
+++ packages/rpc-core/src/rpc-subscriptions/__typetests__/slot-notifications-type-test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
-import { RpcSubscriptions } from '@solana/rpc-transport/dist/types/json-rpc-types';
+import type { RpcSubscriptions } from '@solana/rpc-transport';
 
 import { Slot } from '../../rpc-methods/common';
 import { SolanaRpcSubscriptions } from '../index';
