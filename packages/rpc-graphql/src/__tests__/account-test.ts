b50839283d3b3380301075e7d65f61e3fa75d86f
Callum McIntyre <callum.mcintyre@solana.com>
2023-11-27 11:43:13 +0000

    refactor(experimental): use exported types for rpc-graphql account-test.ts

--- packages/rpc-graphql/src/__tests__/account-test.ts
+++ packages/rpc-graphql/src/__tests__/account-test.ts
@@ -1,6 +1,5 @@
 import { createSolanaRpcApi, SolanaRpcMethods } from '@solana/rpc-core';
-import { createHttpTransport, createJsonRpc } from '@solana/rpc-transport';
-import { Rpc } from '@solana/rpc-transport/dist/types/json-rpc-types';
+import { createHttpTransport, createJsonRpc, type Rpc } from '@solana/rpc-transport';
 import fetchMock from 'jest-fetch-mock-fork';
 
 import { createRpcGraphQL, RpcGraphQL } from '../rpc';
