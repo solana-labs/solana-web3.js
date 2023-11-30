85b7dfe134c660b8a61431e45f35b71c72851507
Steven Luscher <steveluscher@users.noreply.github.com>
2023-11-30 03:42:52 -0800

    refactor(experimental): a `createPrivateKeyFromBytes` helper (#1913)

--- packages/keys/src/__tests__/signatures-test.ts
+++ packages/keys/src/__tests__/signatures-test.ts
@@ -1,6 +1,7 @@
 import { Encoder } from '@solana/codecs-core';
 import { getBase58Encoder } from '@solana/codecs-strings';
 
+import { createPrivateKeyFromBytes } from '../private-key';
 import { SignatureBytes, signBytes, verifySignature } from '../signatures';
 
 jest.mock('@solana/codecs-strings', () => ({
@@ -14,43 +15,10 @@ const MOCK_DATA_SIGNATURE = new Uint8Array([
     181, 52, 75, 112, 225, 150, 128, 184, 164, 36, 21, 101, 205, 115, 28, 127, 221, 24, 135, 229, 8, 69, 232, 16, 225,
     44, 229, 17, 236, 206, 174, 102, 207, 79, 253, 96, 7, 174, 10,
 ]) as SignatureBytes;
-const MOCK_PKCS8_PRIVATE_KEY =
-    // prettier-ignore
-    new Uint8Array([
-        /**
-         * PKCS#8 header
-         */
-        0x30, // ASN.1 sequence tag
-        0x2e, // Length of sequence (46 more bytes)
-
-            0x02, // ASN.1 integer tag
-            0x01, // Length of integer
-                0x00, // Version number
-
-            0x30, // ASN.1 sequence tag
-            0x05, // Length of sequence
-                0x06, // ASN.1 object identifier tag
-                0x03, // Length of object identifier
-                    // Edwards curve algorithms identifier https://oid-rep.orange-labs.fr/get/1.3.101.112
-                        0x2b, // iso(1) / identified-organization(3) (The first node is multiplied by the decimal 40 and the result is added to the value of the second node)
-                        0x65, // thawte(101)
-                    // Ed25519 identifier
-                        0x70, // id-Ed25519(112)
-
-        /**
-         * Private key payload
-         */
-        0x04, // ASN.1 octet string tag
-        0x22, // String length (34 more bytes)
-
-            // Private key bytes as octet string
-            0x04, // ASN.1 octet string tag
-            0x20, // String length (32 bytes)
-                0xeb, 0xfa, 0x65, 0xeb, 0x93, 0xdc, 0x79, 0x15,
-                0x7a, 0xba, 0xde, 0xa2, 0xf7, 0x94, 0x37, 0x9d,
-                0xfc, 0x07, 0x1d, 0x68, 0x86, 0x87, 0x37, 0x6d,
-                0xc5, 0xd5, 0xa0, 0x54, 0x12, 0x1d, 0x34, 0x4a,
-    ]);
+const MOCK_PRIVATE_KEY_BYTES = new Uint8Array([
+    0xeb, 0xfa, 0x65, 0xeb, 0x93, 0xdc, 0x79, 0x15, 0x7a, 0xba, 0xde, 0xa2, 0xf7, 0x94, 0x37, 0x9d, 0xfc, 0x07, 0x1d,
+    0x68, 0x86, 0x87, 0x37, 0x6d, 0xc5, 0xd5, 0xa0, 0x54, 0x12, 0x1d, 0x34, 0x4a,
+]);
 const MOCK_PUBLIC_KEY_BYTES = new Uint8Array([
     0x1d, 0x0e, 0x93, 0x86, 0x4d, 0xcc, 0x81, 0x5f, 0xc3, 0xf2, 0x86, 0x18, 0x09, 0x11, 0xd0, 0x0a, 0x3f, 0xd2, 0x06,
     0xde, 0x31, 0xa1, 0xc9, 0x42, 0x87, 0xcb, 0x43, 0xf0, 0x5f, 0xc9, 0xf2, 0xb5,
@@ -173,13 +141,7 @@ describe('assertIsSignature()', () => {
 describe('sign', () => {
     it('produces the expected signature given a private key', async () => {
         expect.assertions(1);
-        const privateKey = await crypto.subtle.importKey(
-            'pkcs8',
-            MOCK_PKCS8_PRIVATE_KEY,
-            'Ed25519',
-            /* extractable */ false,
-            ['sign'],
-        );
+        const privateKey = await createPrivateKeyFromBytes(MOCK_PRIVATE_KEY_BYTES, /* extractable */ false);
         const signature = await signBytes(privateKey, MOCK_DATA);
         expect(signature).toEqual(MOCK_DATA_SIGNATURE);
     });
