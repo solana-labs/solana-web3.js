e16d21b34e2f531c4f3c58ec6729848133bc9153
Callum <callum.mcintyre@solana.com>
2024-01-11 18:48:42 +0000

    Add stake JSON parsed account types

---
+++ packages/rpc-parsed-types/src/stake-accounts.ts
@@ -0,0 +1,31 @@
+import { Address } from '@solana/addresses';
+import { StringifiedBigInt, UnixTimestamp } from '@solana/rpc-types';
+
+import { RpcParsedType } from './rpc-parsed-type';
+
+type StakeAccount = Readonly<{
+    meta: Readonly<{
+        rentExemptReserve: StringifiedBigInt;
+        authorized: Readonly<{
+            staker: Address;
+            withdrawer: Address;
+        }>;
+        lockup: Readonly<{
+            unixTimestamp: UnixTimestamp;
+            epoch: bigint;
+            custodian: Address;
+        }>;
+    }>;
+    stake: Readonly<{
+        delegation: Readonly<{
+            voter: Address;
+            stake: StringifiedBigInt;
+            activationEpoch: StringifiedBigInt;
+            deactivationEpoch: StringifiedBigInt;
+            warmupCooldownRate: number;
+        }>;
+        creditsObserved: bigint;
+    }> | null;
+}>;
+
+export type StakeProgramAccount = RpcParsedType<'initialized', StakeAccount> | RpcParsedType<'delegated', StakeAccount>;
