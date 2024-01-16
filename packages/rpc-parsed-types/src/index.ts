6d93188883a94713fc2e8f526e68bee62a5803f4
Callum McIntyre <callum.mcintyre@solana.com>
2024-01-16 15:15:23 +0000

    refactor(experimental): export RpcParsedType from rpc-parsed-types package (#2040)

--- packages/rpc-parsed-types/src/index.ts
+++ packages/rpc-parsed-types/src/index.ts
@@ -2,6 +2,7 @@ export * from './address-lookup-table-accounts';
 export * from './bpf-upgradeable-loader-accounts';
 export * from './config-accounts';
 export * from './nonce-accounts';
+export * from './rpc-parsed-type';
 export * from './stake-accounts';
 export * from './sysvar-accounts';
 export * from './token-accounts';
