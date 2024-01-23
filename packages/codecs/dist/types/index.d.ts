2861b43e0d0114e046d0b19d3cf17369ca3097c1
steveluscher <me+github@steveluscher.com>
2024-01-23 02:27:48 +0000

    [NOT FOR LAND] Example of batching account loader

---
+++ packages/codecs/dist/types/index.d.ts
@@ -0,0 +1,6 @@
+export * from '@solana/codecs-core';
+export * from '@solana/codecs-data-structures';
+export * from '@solana/codecs-numbers';
+export * from '@solana/codecs-strings';
+export * from '@solana/options';
+//# sourceMappingURL=index.d.ts.map
--- packages/rpc-graphql/src/context/loaders/account.ts
+++ packages/rpc-graphql/src/context/loaders/account.ts
@@ -1,5 +1,5 @@
 import { Address } from '@solana/addresses';
-import { DataSlice, GetAccountInfoApi, Slot } from '@solana/rpc-core';
+import { DataSlice, GetAccountInfoApi, GetMultipleAccountsApi, Slot } from '@solana/rpc-core';
 import { Rpc } from '@solana/rpc-transport';
 import DataLoader from 'dataloader';
 import { GraphQLResolveInfo } from 'graphql';
@@ -39,14 +39,109 @@ async function loadAccount(rpc: Rpc<GetAccountInfoApi>, { address, ...config }: 
     return account === null ? { address } : transformLoadedAccount({ account, address, encoding: config.encoding });
 }
 
-function createAccountBatchLoadFn(rpc: Rpc<GetAccountInfoApi>) {
+function createAccountBatchLoadFn(rpc: Rpc<GetAccountInfoApi & GetMultipleAccountsApi>) {
     const resolveAccountUsingRpc = loadAccount.bind(null, rpc);
     return async (accountQueryArgs: readonly ReturnType<typeof normalizeArgs>[]) => {
-        return await Promise.all(accountQueryArgs.map(async args => await resolveAccountUsingRpc(args)));
+        const accountsToFetch: {
+            [address: string]: Readonly<{
+                args: Omit<ReturnType<typeof normalizeArgs>, 'address'>;
+                promiseCallbacks: Readonly<{ resolve: (value: unknown) => void; reject: (reason?: unknown) => void }>;
+            }>[];
+        } = {};
+        try {
+            return accountQueryArgs.map(
+                ({ address, ...args }) =>
+                    new Promise((resolve, reject) => {
+                        const accountRecords = (accountsToFetch[address] ||= []);
+                        accountRecords.push({ args, promiseCallbacks: { reject, resolve } });
+                    }),
+            );
+        } finally {
+            /**
+             * STEP 1
+             * Group together accounts that are fetched with identical args.
+             */
+            const accountFetchesByArgsHash: {
+                [argsHash: string]: {
+                    args: Omit<ReturnType<typeof normalizeArgs>, 'address'>;
+                    addresses: {
+                        [address: string]: Readonly<{
+                            resolve: (value: unknown) => void;
+                            reject: (reason?: unknown) => void;
+                        }>[];
+                    };
+                };
+            } = {};
+            Object.entries(accountsToFetch).forEach(([address, fetches]) => {
+                fetches.forEach(({ args, promiseCallbacks }) => {
+                    const argsHash = cacheKeyFn(args);
+                    const accountFetches = (accountFetchesByArgsHash[argsHash] ||= { addresses: {}, args });
+                    const promiseCallbacksForAddress = (accountFetches.addresses[address] ||= []);
+                    promiseCallbacksForAddress.push(promiseCallbacks);
+                });
+            });
+
+            /**
+             * STEP 2 (TODO)
+             * Collapse any fetches for the *same* account but *disjoint* data slices into one fetch
+             * that encompases the union of all the data slices, but only if fetching the union of
+             * the slices would not result in too much overfetching, for some definition of too
+             * much. Done well, you can fetch the account once then parcel the right slice of data
+             * out to the right consumers.
+             */
+
+            /**
+             * STEP 3
+             * For each set of accounts related to some common args, fetch them in the fewest number
+             * of network requests.
+             */
+            Object.values(accountFetchesByArgsHash).forEach(async ({ args, addresses: addressCallbacks }) => {
+                const addresses = Object.keys(addressCallbacks);
+                if (addresses.length === 1) {
+                    const address = addresses[0] as Address;
+                    try {
+                        const result = await resolveAccountUsingRpc({ address, ...args });
+                        addressCallbacks[address].forEach(({ resolve }) => {
+                            resolve(result);
+                        });
+                    } catch (e) {
+                        addressCallbacks[address].forEach(({ reject }) => {
+                            reject(e);
+                        });
+                    }
+                } else {
+                    try {
+                        // TODO: Don't let these batches grow unbounded. Split them into multiple
+                        // `getMultipleAccounts` calls when they grow beyond X addresses.
+                        const { value: results } = await rpc.getMultipleAccounts(addresses as Address[], args).send();
+                        addresses.forEach((address, ii) => {
+                            const result = results[ii];
+                            addressCallbacks[address].forEach(({ resolve }) => {
+                                resolve(
+                                    result === null
+                                        ? { address }
+                                        : transformLoadedAccount({
+                                              account: result,
+                                              address: address as Address,
+                                              encoding: args.encoding,
+                                          }),
+                                );
+                            });
+                        });
+                    } catch (e) {
+                        addresses.forEach(address => {
+                            addressCallbacks[address].forEach(({ reject }) => {
+                                reject(e);
+                            });
+                        });
+                    }
+                }
+            });
+        }
     };
 }
 
-export function createAccountLoader(rpc: Rpc<GetAccountInfoApi>) {
+export function createAccountLoader(rpc: Rpc<GetAccountInfoApi & GetMultipleAccountsApi>) {
     const loader = new DataLoader(createAccountBatchLoadFn(rpc), { cacheKeyFn });
     return {
         load: async (args: AccountLoaderArgs, info?: GraphQLResolveInfo) => {
