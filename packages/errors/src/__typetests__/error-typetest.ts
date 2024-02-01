8e39a428c591240b32039acdd68a7fdaea8aae60
Steven Luscher <steveluscher@users.noreply.github.com>
2024-02-01 11:12:27 -0800

    A typetest for coded errors (#2077)
    # Summary
    
    This typetest really drives home how to use `SolanaError` and `isSolanaError()` in practice. Watch how you can refine errors by code, and how the type of the context materiailzes with it.
    
    # Test Plan
    
    ```shell
    cd packages/errors/
    pnpm turbo test:typecheck
    ```

---
+++ packages/errors/src/__typetests__/error-typetest.ts
@@ -0,0 +1,46 @@
+import { address } from '@solana/addresses';
+
+import * as SolanaErrorCodeModule from '../codes';
+import { SolanaErrorCode } from '../codes';
+import { SolanaErrorContext } from '../context';
+import { isSolanaError, SolanaError } from '../error';
+
+const { SOLANA_ERROR__TRANSACTION_SIGNATURE_NOT_COMPUTABLE, SOLANA_ERROR__TRANSACTION_MISSING_SIGNATURES } =
+    SolanaErrorCodeModule;
+
+// If this line raises a type error, you might have forgotten to add a new error to the
+// `SolanaErrorCode` union in `src/codes.ts`.
+Object.values(SolanaErrorCodeModule) satisfies SolanaErrorCode[];
+
+const transactionMissingSignaturesError = new SolanaError(SOLANA_ERROR__TRANSACTION_MISSING_SIGNATURES, {
+    addresses: [address('123'), '456'],
+});
+
+transactionMissingSignaturesError.context.__code satisfies typeof SOLANA_ERROR__TRANSACTION_MISSING_SIGNATURES;
+// @ts-expect-error Wrong error code.
+transactionMissingSignaturesError.context.__code satisfies typeof SOLANA_ERROR__TRANSACTION_SIGNATURE_NOT_COMPUTABLE;
+
+transactionMissingSignaturesError.context satisfies SolanaErrorContext[typeof SOLANA_ERROR__TRANSACTION_MISSING_SIGNATURES];
+// @ts-expect-error Non existent context property.
+transactionMissingSignaturesError.context.feePayer;
+
+new SolanaError(SOLANA_ERROR__TRANSACTION_SIGNATURE_NOT_COMPUTABLE);
+// @ts-expect-error Missing context property (`addresses`)
+new SolanaError(SOLANA_ERROR__TRANSACTION_MISSING_SIGNATURES);
+
+const unknownError = null as unknown as SolanaError;
+if (unknownError.context.__code === SOLANA_ERROR__TRANSACTION_MISSING_SIGNATURES) {
+    unknownError.context satisfies SolanaErrorContext[typeof SOLANA_ERROR__TRANSACTION_MISSING_SIGNATURES];
+    // @ts-expect-error Context belongs to another error code
+    unknownError.context satisfies SolanaErrorContext[typeof SOLANA_ERROR__TRANSACTION_SIGNATURE_NOT_COMPUTABLE];
+}
+
+const e = null as unknown;
+if (isSolanaError(e)) {
+    e.context satisfies Readonly<{ __code: SolanaErrorCode }>;
+}
+if (isSolanaError(e, SOLANA_ERROR__TRANSACTION_MISSING_SIGNATURES)) {
+    e.context satisfies SolanaErrorContext[typeof SOLANA_ERROR__TRANSACTION_MISSING_SIGNATURES];
+    // @ts-expect-error Context belongs to another error code
+    e.context satisfies SolanaErrorContext[typeof SOLANA_ERROR__TRANSACTION_SIGNATURE_NOT_COMPUTABLE];
+}
