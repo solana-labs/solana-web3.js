0dc7a6d53d2284269063953ab2ff5524e725c79d
Joe <jcaulfield135@gmail.com>
2024-01-31 21:11:42 -0600

    refactor(experimental): add `ClusterUrl` types

---
+++ packages/rpc-types/src/cluster-url.ts
@@ -0,0 +1,14 @@
+export type MainnetUrl = string & { '~cluster': 'mainnet' };
+export type DevnetUrl = string & { '~cluster': 'devnet' };
+export type TestnetUrl = string & { '~cluster': 'testnet' };
+export type ClusterUrl = string | MainnetUrl | DevnetUrl | TestnetUrl;
+
+export function mainnet(putativeString: string): MainnetUrl {
+    return putativeString as MainnetUrl;
+}
+export function devnet(putativeString: string): DevnetUrl {
+    return putativeString as DevnetUrl;
+}
+export function testnet(putativeString: string): TestnetUrl {
+    return putativeString as TestnetUrl;
+}
