0ec0a28b93456e3c43621e65b51e967157aeba47
steveluscher <me+github@steveluscher.com>
2024-02-03 00:06:06 +0000

    Convert `SolanaRpcIntegerOverflowError` to a coded error

--- packages/library/src/__tests__/rpc-integer-overflow-test.ts
+++ packages/library/src/__tests__/rpc-integer-overflow-test.ts
@@ -1,7 +1,7 @@
+import { SolanaError } from '@solana/errors';
 import type { IRpcTransport } from '@solana/rpc-transport';
 
 import { createSolanaRpc } from '../rpc';
-import { SolanaJsonRpcIntegerOverflowError } from '../rpc-integer-overflow-error';
 
 describe('RPC integer overflow behavior', () => {
     let rpc: ReturnType<typeof createSolanaRpc>;
@@ -27,11 +27,11 @@ describe('RPC integer overflow behavior', () => {
     it('throws when called with a value greater than `Number.MAX_SAFE_INTEGER`', () => {
         expect(() => {
             rpc.getBlocks(BigInt(Number.MAX_SAFE_INTEGER) + 1n);
-        }).toThrow(SolanaJsonRpcIntegerOverflowError);
+        }).toThrow(SolanaError);
     });
     it('throws when called with a value less than `-Number.MAX_SAFE_INTEGER`', () => {
         expect(() => {
             rpc.getBlocks(BigInt(-Number.MAX_SAFE_INTEGER) - 1n);
-        }).toThrow(SolanaJsonRpcIntegerOverflowError);
+        }).toThrow(SolanaError);
     });
 });
--- packages/library/src/rpc-integer-overflow-error.ts
+++ packages/library/src/rpc-integer-overflow-error.ts
@@ -1,45 +1,45 @@
+import { SOLANA_ERROR__RPC_INTEGER_OVERFLOW, SolanaError } from '@solana/errors';
 import { KeyPath } from '@solana/rpc-core/dist/types/tree-traversal';
 
-export class SolanaJsonRpcIntegerOverflowError extends Error {
-    readonly methodName: string;
-    readonly keyPath: KeyPath;
-    readonly value: bigint;
-    constructor(methodName: string, keyPath: KeyPath, value: bigint) {
-        let argumentLabel = '';
-        if (typeof keyPath[0] === 'number') {
-            const argPosition = keyPath[0] + 1;
-            const lastDigit = argPosition % 10;
-            const lastTwoDigits = argPosition % 100;
-            if (lastDigit == 1 && lastTwoDigits != 11) {
-                argumentLabel = argPosition + 'st';
-            } else if (lastDigit == 2 && lastTwoDigits != 12) {
-                argumentLabel = argPosition + 'nd';
-            } else if (lastDigit == 3 && lastTwoDigits != 13) {
-                argumentLabel = argPosition + 'rd';
-            } else {
-                argumentLabel = argPosition + 'th';
-            }
+export function createSolanaJsonRpcIntegerOverflowError(
+    methodName: string,
+    keyPath: KeyPath,
+    value: bigint,
+): SolanaError<typeof SOLANA_ERROR__RPC_INTEGER_OVERFLOW> {
+    let argumentLabel = '';
+    if (typeof keyPath[0] === 'number') {
+        const argPosition = keyPath[0] + 1;
+        const lastDigit = argPosition % 10;
+        const lastTwoDigits = argPosition % 100;
+        if (lastDigit == 1 && lastTwoDigits != 11) {
+            argumentLabel = argPosition + 'st';
+        } else if (lastDigit == 2 && lastTwoDigits != 12) {
+            argumentLabel = argPosition + 'nd';
+        } else if (lastDigit == 3 && lastTwoDigits != 13) {
+            argumentLabel = argPosition + 'rd';
         } else {
-            argumentLabel = `\`${keyPath[0].toString()}\``;
+            argumentLabel = argPosition + 'th';
         }
-        const path =
-            keyPath.length > 1
-                ? keyPath
-                      .slice(1)
-                      .map(pathPart => (typeof pathPart === 'number' ? `[${pathPart}]` : pathPart))
-                      .join('.')
-                : null;
-        super(
-            `The ${argumentLabel} argument to the \`${methodName}\` RPC method` +
-                `${path ? ` at path \`${path}\`` : ''} was \`${value}\`. This number is ` +
-                'unsafe for use with the Solana JSON-RPC because it exceeds ' +
-                '`Number.MAX_SAFE_INTEGER`.',
-        );
-        this.keyPath = keyPath;
-        this.methodName = methodName;
-        this.value = value;
+    } else {
+        argumentLabel = `\`${keyPath[0].toString()}\``;
     }
-    get name() {
-        return 'SolanaJsonRpcIntegerOverflowError';
+    const path =
+        keyPath.length > 1
+            ? keyPath
+                  .slice(1)
+                  .map(pathPart => (typeof pathPart === 'number' ? `[${pathPart}]` : pathPart))
+                  .join('.')
+            : undefined;
+    const error = new SolanaError(SOLANA_ERROR__RPC_INTEGER_OVERFLOW, {
+        argumentLabel,
+        keyPath: keyPath as readonly (string | number | symbol)[],
+        methodName,
+        optionalPathLabel: path ? ` at path \`${path}\`` : '',
+        value,
+        ...(path !== undefined ? { path } : undefined),
+    });
+    if ('captureStackTrace' in Error && typeof Error.captureStackTrace === 'function') {
+        Error.captureStackTrace(error, createSolanaJsonRpcIntegerOverflowError);
     }
+    return error;
 }
