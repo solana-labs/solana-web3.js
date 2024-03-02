38156b3b9df6b7d45c9940120ea85b18813a19fc
Loris Leiva <loris.leiva@gmail.com>
2024-03-02 15:40:59 +0000

    refactor(experimental): add function to create a Solana RPC using only an URL

--- packages/rpc/src/__typetests__/rpc-clusters-typetest.ts
+++ packages/rpc/src/__typetests__/rpc-clusters-typetest.ts
@@ -2,7 +2,7 @@ import type { SolanaRpcApi, SolanaRpcApiDevnet, SolanaRpcApiMainnet, SolanaRpcAp
 import type { Rpc, RpcTransport } from '@solana/rpc-spec';
 import { devnet, mainnet, testnet } from '@solana/rpc-types';
 
-import { createSolanaRpc } from '../rpc';
+import { createSolanaRpc, createSolanaRpcFromTransport } from '../rpc';
 import type {
     RpcDevnet,
     RpcMainnet,
@@ -13,85 +13,165 @@ import type {
 } from '../rpc-clusters';
 import { createDefaultRpcTransport } from '../rpc-transport';
 
-// Creating default HTTP transports
+// Define cluster-aware URLs and transports.
 
 const genericUrl = 'http://localhost:8899';
 const devnetUrl = devnet('https://api.devnet.solana.com');
 const testnetUrl = testnet('https://api.testnet.solana.com');
 const mainnetUrl = mainnet('https://api.mainnet-beta.solana.com');
 
-// No cluster specified should be generic `RpcTransport`
-createDefaultRpcTransport({ url: genericUrl }) satisfies RpcTransport;
-//@ts-expect-error Should not be a devnet transport
-createDefaultRpcTransport({ url: genericUrl }) satisfies RpcTransportDevnet;
-//@ts-expect-error Should not be a testnet transport
-createDefaultRpcTransport({ url: genericUrl }) satisfies RpcTransportTestnet;
-//@ts-expect-error Should not be a mainnet transport
-createDefaultRpcTransport({ url: genericUrl }) satisfies RpcTransportMainnet;
-
-// Devnet cluster should be `RpcTransportDevnet`
-createDefaultRpcTransport({ url: devnetUrl }) satisfies RpcTransportDevnet;
-//@ts-expect-error Should not be a testnet transport
-createDefaultRpcTransport({ url: devnetUrl }) satisfies RpcTransportTestnet;
-//@ts-expect-error Should not be a mainnet transport
-createDefaultRpcTransport({ url: devnetUrl }) satisfies RpcTransportMainnet;
-
-// Testnet cluster should be `RpcTransportTestnet`
-createDefaultRpcTransport({ url: testnetUrl }) satisfies RpcTransportTestnet;
-//@ts-expect-error Should not be a devnet transport
-createDefaultRpcTransport({ url: testnetUrl }) satisfies RpcTransportDevnet;
-//@ts-expect-error Should not be a mainnet transport
-createDefaultRpcTransport({ url: testnetUrl }) satisfies RpcTransportMainnet;
-
-// Mainnet cluster should be `RpcTransportMainnet`
-createDefaultRpcTransport({ url: mainnetUrl }) satisfies RpcTransportMainnet;
-//@ts-expect-error Should not be a devnet transport
-createDefaultRpcTransport({ url: mainnetUrl }) satisfies RpcTransportDevnet;
-//@ts-expect-error Should not be a testnet transport
-createDefaultRpcTransport({ url: mainnetUrl }) satisfies RpcTransportTestnet;
-
-// Creating JSON RPC clients
-
 const genericTransport = createDefaultRpcTransport({ url: genericUrl });
 const devnetTransport = createDefaultRpcTransport({ url: devnetUrl });
 const testnetTransport = createDefaultRpcTransport({ url: testnetUrl });
 const mainnetTransport = createDefaultRpcTransport({ url: mainnetUrl });
 
-// No cluster specified should be generic `Rpc`
-createSolanaRpc({ transport: genericTransport }) satisfies Rpc<SolanaRpcApi>;
-//@ts-expect-error Should not be a devnet RPC
-createSolanaRpc({ transport: genericTransport }) satisfies RpcDevnet<SolanaRpcApi>;
-//@ts-expect-error Should not be a testnet RPC
-createSolanaRpc({ transport: genericTransport }) satisfies RpcTestnet<SolanaRpcApi>;
-//@ts-expect-error Should not be a mainnet RPC
-createSolanaRpc({ transport: genericTransport }) satisfies RpcMainnet<SolanaRpcApi>;
+// [DEFINE] createDefaultRpcTransport.
+{
+    // No cluster specified should be generic `RpcTransport`.
+    {
+        genericTransport satisfies RpcTransport;
+        //@ts-expect-error Should not be a devnet transport
+        genericTransport satisfies RpcTransportDevnet;
+        //@ts-expect-error Should not be a testnet transport
+        genericTransport satisfies RpcTransportTestnet;
+        //@ts-expect-error Should not be a mainnet transport
+        genericTransport satisfies RpcTransportMainnet;
+    }
 
-// Devnet cluster should be `RpcDevnet`
-createSolanaRpc({ transport: devnetTransport }) satisfies Rpc<SolanaRpcApi>;
-createSolanaRpc({ transport: devnetTransport }) satisfies Rpc<SolanaRpcApiDevnet>;
-createSolanaRpc({ transport: devnetTransport }) satisfies RpcDevnet<SolanaRpcApi>;
-createSolanaRpc({ transport: devnetTransport }) satisfies RpcDevnet<SolanaRpcApiDevnet>; // Same types
-//@ts-expect-error Should not be a testnet RPC
-createSolanaRpc({ transport: devnetTransport }) satisfies RpcTestnet<SolanaRpcApi>;
-//@ts-expect-error Should not be a mainnet RPC
-createSolanaRpc({ transport: devnetTransport }) satisfies RpcMainnet<SolanaRpcApi>;
+    // Devnet cluster should be `RpcTransportDevnet`.
+    {
+        devnetTransport satisfies RpcTransportDevnet;
+        //@ts-expect-error Should not be a testnet transport
+        devnetTransport satisfies RpcTransportTestnet;
+        //@ts-expect-error Should not be a mainnet transport
+        devnetTransport satisfies RpcTransportMainnet;
+    }
 
-// Testnet cluster should be `RpcTestnet`
-createSolanaRpc({ transport: testnetTransport }) satisfies Rpc<SolanaRpcApi>;
-createSolanaRpc({ transport: testnetTransport }) satisfies Rpc<SolanaRpcApiTestnet>;
-createSolanaRpc({ transport: testnetTransport }) satisfies RpcTestnet<SolanaRpcApi>;
-createSolanaRpc({ transport: testnetTransport }) satisfies RpcTestnet<SolanaRpcApiTestnet>; // Same types
-//@ts-expect-error Should not be a devnet RPC
-createSolanaRpc({ transport: testnetTransport }) satisfies RpcDevnet<SolanaRpcApi>;
-//@ts-expect-error Should not be a mainnet RPC
-createSolanaRpc({ transport: testnetTransport }) satisfies RpcMainnet<SolanaRpcApi>;
+    // Testnet cluster should be `RpcTransportTestnet`.
+    {
+        testnetTransport satisfies RpcTransportTestnet;
+        //@ts-expect-error Should not be a devnet transport
+        testnetTransport satisfies RpcTransportDevnet;
+        //@ts-expect-error Should not be a mainnet transport
+        testnetTransport satisfies RpcTransportMainnet;
+    }
 
-// Mainnet cluster should be `RpcMainnet`
-createSolanaRpc({ transport: mainnetTransport }) satisfies Rpc<SolanaRpcApiMainnet>;
-createSolanaRpc({ transport: mainnetTransport }) satisfies RpcMainnet<SolanaRpcApiMainnet>;
-//@ts-expect-error Should not have `requestAirdrop` method
-createSolanaRpc({ transport: mainnetTransport }) satisfies Rpc<RequestAirdropApi>;
-//@ts-expect-error Should not be a devnet RPC
-createSolanaRpc({ transport: mainnetTransport }) satisfies RpcDevnet<SolanaRpcApi>;
-//@ts-expect-error Should not be a testnet RPC
-createSolanaRpc({ transport: mainnetTransport }) satisfies RpcTestnet<SolanaRpcApi>;
+    // Mainnet cluster should be `RpcTransportMainnet`.
+    {
+        mainnetTransport satisfies RpcTransportMainnet;
+        //@ts-expect-error Should not be a devnet transport
+        mainnetTransport satisfies RpcTransportDevnet;
+        //@ts-expect-error Should not be a testnet transport
+        mainnetTransport satisfies RpcTransportTestnet;
+    }
+}
+
+// [DEFINE] createSolanaRpcFromTransport.
+{
+    const genericRpc = createSolanaRpcFromTransport(genericTransport);
+    const devnetRpc = createSolanaRpcFromTransport(devnetTransport);
+    const testnetRpc = createSolanaRpcFromTransport(testnetTransport);
+    const mainnetRpc = createSolanaRpcFromTransport(mainnetTransport);
+
+    // No cluster specified should be generic `Rpc`.
+    {
+        genericRpc satisfies Rpc<SolanaRpcApi>;
+        //@ts-expect-error Should not be a devnet RPC
+        genericRpc satisfies RpcDevnet<SolanaRpcApi>;
+        //@ts-expect-error Should not be a testnet RPC
+        genericRpc satisfies RpcTestnet<SolanaRpcApi>;
+        //@ts-expect-error Should not be a mainnet RPC
+        genericRpc satisfies RpcMainnet<SolanaRpcApi>;
+    }
+
+    // Devnet cluster should be `RpcDevnet`.
+    {
+        devnetRpc satisfies Rpc<SolanaRpcApi>;
+        devnetRpc satisfies Rpc<SolanaRpcApiDevnet>;
+        devnetRpc satisfies RpcDevnet<SolanaRpcApi>;
+        devnetRpc satisfies RpcDevnet<SolanaRpcApiDevnet>; // Same types
+        //@ts-expect-error Should not be a testnet RPC
+        devnetRpc satisfies RpcTestnet<SolanaRpcApi>;
+        //@ts-expect-error Should not be a mainnet RPC
+        devnetRpc satisfies RpcMainnet<SolanaRpcApi>;
+    }
+
+    // Testnet cluster should be `RpcTestnet`.
+    {
+        testnetRpc satisfies Rpc<SolanaRpcApi>;
+        testnetRpc satisfies Rpc<SolanaRpcApiTestnet>;
+        testnetRpc satisfies RpcTestnet<SolanaRpcApi>;
+        testnetRpc satisfies RpcTestnet<SolanaRpcApiTestnet>; // Same types
+        //@ts-expect-error Should not be a devnet RPC
+        testnetRpc satisfies RpcDevnet<SolanaRpcApi>;
+        //@ts-expect-error Should not be a mainnet RPC
+        testnetRpc satisfies RpcMainnet<SolanaRpcApi>;
+    }
+
+    // Mainnet cluster should be `RpcMainnet`.
+    {
+        mainnetRpc satisfies Rpc<SolanaRpcApiMainnet>;
+        mainnetRpc satisfies RpcMainnet<SolanaRpcApiMainnet>;
+        //@ts-expect-error Should not have `requestAirdrop` method
+        mainnetRpc satisfies Rpc<RequestAirdropApi>;
+        //@ts-expect-error Should not be a devnet RPC
+        mainnetRpc satisfies RpcDevnet<SolanaRpcApi>;
+        //@ts-expect-error Should not be a testnet RPC
+        mainnetRpc satisfies RpcTestnet<SolanaRpcApi>;
+    }
+}
+
+// [DEFINE] createSolanaRpc.
+{
+    const genericRpc = createSolanaRpc(genericUrl);
+    const devnetRpc = createSolanaRpc(devnetUrl);
+    const testnetRpc = createSolanaRpc(testnetUrl);
+    const mainnetRpc = createSolanaRpc(mainnetUrl);
+
+    // No cluster specified should be generic `Rpc`.
+    {
+        genericRpc satisfies Rpc<SolanaRpcApi>;
+        //@ts-expect-error Should not be a devnet RPC
+        genericRpc satisfies RpcDevnet<SolanaRpcApi>;
+        //@ts-expect-error Should not be a testnet RPC
+        genericRpc satisfies RpcTestnet<SolanaRpcApi>;
+        //@ts-expect-error Should not be a mainnet RPC
+        genericRpc satisfies RpcMainnet<SolanaRpcApi>;
+    }
+
+    // Devnet cluster should be `RpcDevnet`.
+    {
+        devnetRpc satisfies Rpc<SolanaRpcApi>;
+        devnetRpc satisfies Rpc<SolanaRpcApiDevnet>;
+        devnetRpc satisfies RpcDevnet<SolanaRpcApi>;
+        devnetRpc satisfies RpcDevnet<SolanaRpcApiDevnet>; // Same types
+        //@ts-expect-error Should not be a testnet RPC
+        devnetRpc satisfies RpcTestnet<SolanaRpcApi>;
+        //@ts-expect-error Should not be a mainnet RPC
+        devnetRpc satisfies RpcMainnet<SolanaRpcApi>;
+    }
+
+    // Testnet cluster should be `RpcTestnet`.
+    {
+        testnetRpc satisfies Rpc<SolanaRpcApi>;
+        testnetRpc satisfies Rpc<SolanaRpcApiTestnet>;
+        testnetRpc satisfies RpcTestnet<SolanaRpcApi>;
+        testnetRpc satisfies RpcTestnet<SolanaRpcApiTestnet>; // Same types
+        //@ts-expect-error Should not be a devnet RPC
+        testnetRpc satisfies RpcDevnet<SolanaRpcApi>;
+        //@ts-expect-error Should not be a mainnet RPC
+        testnetRpc satisfies RpcMainnet<SolanaRpcApi>;
+    }
+
+    // Mainnet cluster should be `RpcMainnet`.
+    {
+        mainnetRpc satisfies Rpc<SolanaRpcApiMainnet>;
+        mainnetRpc satisfies RpcMainnet<SolanaRpcApiMainnet>;
+        //@ts-expect-error Should not have `requestAirdrop` method
+        mainnetRpc satisfies Rpc<RequestAirdropApi>;
+        //@ts-expect-error Should not be a devnet RPC
+        mainnetRpc satisfies RpcDevnet<SolanaRpcApi>;
+        //@ts-expect-error Should not be a testnet RPC
+        mainnetRpc satisfies RpcTestnet<SolanaRpcApi>;
+    }
+}
--- packages/rpc/src/rpc.ts
+++ packages/rpc/src/rpc.ts
@@ -1,16 +1,23 @@
 import { createSolanaRpcApi } from '@solana/rpc-api';
 import { createRpc, RpcTransport } from '@solana/rpc-spec';
+import { ClusterUrl } from '@solana/rpc-types';
 
 import type { RpcFromTransport, SolanaRpcApiFromTransport } from './rpc-clusters';
 import { DEFAULT_RPC_CONFIG } from './rpc-default-config';
+import { createDefaultRpcTransport, DefaultRpcTransportConfig } from './rpc-transport';
 
-type RpcConfig<TTransport extends RpcTransport> = Readonly<{
-    transport: TTransport;
-}>;
+/** Creates a new Solana RPC using the default decorated HTTP transport. */
+export function createSolanaRpc<TClusterUrl extends ClusterUrl>(
+    clusterUrl: TClusterUrl,
+    config?: Omit<DefaultRpcTransportConfig<TClusterUrl>, 'url'>,
+) {
+    return createSolanaRpcFromTransport(createDefaultRpcTransport({ url: clusterUrl, ...config }));
+}
 
-export function createSolanaRpc<TTransport extends RpcTransport>(
-    config: RpcConfig<TTransport>,
-): RpcFromTransport<SolanaRpcApiFromTransport<TTransport>, TTransport> {
-    const api = createSolanaRpcApi(DEFAULT_RPC_CONFIG);
-    return createRpc({ ...config, api }) as RpcFromTransport<SolanaRpcApiFromTransport<TTransport>, TTransport>;
+/** Creates a new Solana RPC using a custom transport. */
+export function createSolanaRpcFromTransport<TTransport extends RpcTransport>(transport: TTransport) {
+    return createRpc({
+        api: createSolanaRpcApi(DEFAULT_RPC_CONFIG),
+        transport,
+    }) as RpcFromTransport<SolanaRpcApiFromTransport<TTransport>, TTransport>;
 }
