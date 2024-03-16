1c3c7c0e5254436a097e94a40b9498b04ce3b8b3
Alex Luu <alex.luu@mail.utoronto.ca>
2024-03-16 14:49:18 -0400

    add testcases and error codes for createKeyPairFromBytes

--- packages/keys/src/key-pair.ts
+++ packages/keys/src/key-pair.ts
@@ -1,8 +1,10 @@
 import { assertKeyGenerationIsAvailable } from '@solana/assertions';
-import { SOLANA_ERROR__KEYS__INVALID_KEY_PAIR_BYTE_LENGTH, SolanaError } from '@solana/errors';
+import { SOLANA_ERROR__KEYS__INVALID_KEY_PAIR_BYTE_LENGTH, SolanaError, SOLANA_ERROR__KEYS__VERIFY_SIGNATURE_FAILURE } from '@solana/errors';
 
 import { createPrivateKeyFromBytes } from './private-key';
 import { verifySignature, signBytes } from './signatures';
+
+const TEST_DATA = new Uint8Array([1, 2, 3, 4, 5]);
 
 export async function generateKeyPair(): Promise<CryptoKeyPair> {
     await assertKeyGenerationIsAvailable();
@@ -22,18 +24,21 @@ export async function createKeyPairFromBytes(bytes: Uint8Array, extractable?: bo
         crypto.subtle.importKey('raw', bytes.slice(32), 'Ed25519', /* extractable */ true, ['verify']),
         createPrivateKeyFromBytes(bytes.slice(0, 32), extractable),
     ]);
-    const signedData = await signBytes(privateKey, new Uint8Array([1, 2, 3, 4, 5]));
-    const isValid = await verifySignature(publicKey, signedData, new Uint8Array([1, 2, 3, 4, 5]));
+
+    // Verify the key pair
+    const signedData = await signBytes(privateKey, TEST_DATA);
+    const isValid = await verifySignature(publicKey, signedData, TEST_DATA);
     if (!isValid) {
-        throw new SolanaError(SOLANA_ERROR__INVALID_KEYPAIR_BYTES);
+        throw new SolanaError(SOLANA_ERROR__KEYS__VERIFY_SIGNATURE_FAILURE);
     }
 
     return { privateKey, publicKey } as CryptoKeyPair;
 }
 
+// Similar to createKeyPairFromBytes, but does not verify the key pair
 export async function createKeyPairFromBytes_DANGEROUSLY_SKIP_VALIDATION(bytes: Uint8Array, extractable?: boolean): Promise<CryptoKeyPair> {
     if (bytes.byteLength !== 64) {
-        throw new SolanaError(SOLANA_ERROR__INVALID_KEYPAIR_BYTES, { byteLength: bytes.byteLength });
+        throw new SolanaError(SOLANA_ERROR__KEYS__INVALID_KEY_PAIR_BYTE_LENGTH, { byteLength: bytes.byteLength });
     }
     const [publicKey, privateKey] = await Promise.all([
         crypto.subtle.importKey('raw', bytes.slice(32), 'Ed25519', /* extractable */ true, ['verify']),
