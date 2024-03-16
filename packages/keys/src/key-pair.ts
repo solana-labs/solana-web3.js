3e8eb1d66c8ecd4bc2e9dc47e4988b80df11465c
Alex Luu <alex.luu@mail.utoronto.ca>
2024-03-16 00:17:53 -0400

    add createKeyPairFromBytes_DANGEROUSLY_SKIP_VALIDATION and validation

--- packages/keys/src/key-pair.ts
+++ packages/keys/src/key-pair.ts
@@ -2,6 +2,7 @@ import { assertKeyGenerationIsAvailable } from '@solana/assertions';
 import { SOLANA_ERROR__KEYS__INVALID_KEY_PAIR_BYTE_LENGTH, SolanaError } from '@solana/errors';
 
 import { createPrivateKeyFromBytes } from './private-key';
+import { verifySignature, signBytes } from './signatures';
 
 export async function generateKeyPair(): Promise<CryptoKeyPair> {
     await assertKeyGenerationIsAvailable();
@@ -21,5 +22,23 @@ export async function createKeyPairFromBytes(bytes: Uint8Array, extractable?: bo
         crypto.subtle.importKey('raw', bytes.slice(32), 'Ed25519', /* extractable */ true, ['verify']),
         createPrivateKeyFromBytes(bytes.slice(0, 32), extractable),
     ]);
+    const signedData = await signBytes(privateKey, new Uint8Array([1, 2, 3, 4, 5]));
+    const isValid = await verifySignature(publicKey, signedData, new Uint8Array([1, 2, 3, 4, 5]));
+    if (!isValid) {
+        throw new SolanaError(SOLANA_ERROR__INVALID_KEYPAIR_BYTES);
+    }
+
+    return { privateKey, publicKey } as CryptoKeyPair;
+}
+
+export async function createKeyPairFromBytes_DANGEROUSLY_SKIP_VALIDATION(bytes: Uint8Array, extractable?: boolean): Promise<CryptoKeyPair> {
+    if (bytes.byteLength !== 64) {
+        throw new SolanaError(SOLANA_ERROR__INVALID_KEYPAIR_BYTES, { byteLength: bytes.byteLength });
+    }
+    const [publicKey, privateKey] = await Promise.all([
+        crypto.subtle.importKey('raw', bytes.slice(32), 'Ed25519', /* extractable */ true, ['verify']),
+        createPrivateKeyFromBytes(bytes.slice(0, 32), extractable),
+    ]);
+
     return { privateKey, publicKey } as CryptoKeyPair;
 }
