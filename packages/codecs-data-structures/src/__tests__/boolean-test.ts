0c939af9ef2c88763f3862f888ca09c7bba75e93
Loris Leiva <loris.leiva@gmail.com>
2024-04-04 10:01:47 +0100

    refactor(experimental): allow variable-size number codecs in getBooleanCodec

--- packages/codecs-data-structures/src/__tests__/boolean-test.ts
+++ packages/codecs-data-structures/src/__tests__/boolean-test.ts
@@ -1,36 +1,84 @@
+import { mapCodec } from '@solana/codecs-core';
 import { getShortU16Codec, getU32Codec } from '@solana/codecs-numbers';
-import { SOLANA_ERROR__CODECS__EXPECTED_FIXED_LENGTH, SolanaError } from '@solana/errors';
 
 import { getBooleanCodec } from '../boolean';
 import { b } from './__setup__';
 
 describe('getBooleanCodec', () => {
-    const boolean = getBooleanCodec;
-    const u32 = getU32Codec;
+    // A variable-size number codecs that uses 0 for `false`
+    // and the max shortU16 value for `true`.
+    const mappedShortU16 = mapCodec(
+        getShortU16Codec(),
+        // eslint-disable-next-line jest/no-conditional-in-test
+        v => (v === 0 ? 0 : 0xffff),
+        // eslint-disable-next-line jest/no-conditional-in-test
+        v => (v === 0 ? 0 : 1),
+    );
 
-    it('encodes booleans', () => {
-        // Encode.
-        expect(boolean().encode(true)).toStrictEqual(b('01'));
-        expect(boolean().encode(false)).toStrictEqual(b('00'));
-        expect(boolean({ size: u32() }).encode(true)).toStrictEqual(b('01000000'));
-        expect(boolean({ size: u32() }).encode(false)).toStrictEqual(b('00000000'));
-
-        // Decode.
-        expect(boolean().read(b('01'), 0)).toStrictEqual([true, 1]);
-        expect(boolean().read(b('00'), 0)).toStrictEqual([false, 1]);
-        expect(boolean().read(b('ffff01'), 2)).toStrictEqual([true, 3]);
-        expect(boolean().read(b('ffff00'), 2)).toStrictEqual([false, 3]);
-        expect(boolean({ size: u32() }).read(b('01000000'), 0)).toStrictEqual([true, 4]);
-        expect(boolean({ size: u32() }).read(b('00000000'), 0)).toStrictEqual([false, 4]);
-
-        // Fails if the codec is not fixed size.
-        expect(() => boolean({ size: getShortU16Codec() })).toThrow(
-            new SolanaError(SOLANA_ERROR__CODECS__EXPECTED_FIXED_LENGTH),
-        );
+    it('encodes booleans using a u8 number', () => {
+        expect(getBooleanCodec().encode(true)).toStrictEqual(b('01'));
+        expect(getBooleanCodec().encode(false)).toStrictEqual(b('00'));
     });
 
-    it('has the right sizes', () => {
-        expect(boolean().fixedSize).toBe(1);
-        expect(boolean({ size: u32() }).fixedSize).toBe(4);
+    it('decodes booleans using a u8 number', () => {
+        expect(getBooleanCodec().decode(b('01'))).toBe(true);
+        expect(getBooleanCodec().decode(b('00'))).toBe(false);
+    });
+
+    it('encodes booleans using a custom fixed-size number codec', () => {
+        const codec = getBooleanCodec({ size: getU32Codec() });
+        expect(codec.encode(true)).toStrictEqual(b('01000000'));
+        expect(codec.encode(false)).toStrictEqual(b('00000000'));
+    });
+
+    it('decodes booleans using a custom fixed-size number codec', () => {
+        const codec = getBooleanCodec({ size: getU32Codec() });
+        expect(codec.decode(b('01000000'))).toBe(true);
+        expect(codec.decode(b('00000000'))).toBe(false);
+    });
+
+    it('encodes booleans using a custom variable-size number codec', () => {
+        const codec = getBooleanCodec({ size: mappedShortU16 });
+        expect(codec.encode(true)).toStrictEqual(b('ffff03'));
+        expect(codec.encode(false)).toStrictEqual(b('00'));
+    });
+
+    it('decodes booleans using a custom variable-size number codec', () => {
+        const codec = getBooleanCodec({ size: mappedShortU16 });
+        expect(codec.decode(b('ffff03'))).toBe(true);
+        expect(codec.decode(b('00'))).toBe(false);
+    });
+
+    it('pushes the offset forward when writing', () => {
+        expect(getBooleanCodec().write(true, new Uint8Array(10), 6)).toBe(7);
+    });
+
+    it('pushes the offset forward when reading', () => {
+        expect(getBooleanCodec().read(b('ffff00'), 2)).toStrictEqual([false, 3]);
+    });
+
+    it('pushes the offset forward when writing using a custom size', () => {
+        expect(getBooleanCodec({ size: getU32Codec() }).write(true, new Uint8Array(10), 3)).toBe(7);
+    });
+
+    it('pushes the offset forward when reading using a custom size', () => {
+        expect(getBooleanCodec({ size: getU32Codec() }).read(b('ffff00000000'), 2)).toStrictEqual([false, 6]);
+    });
+
+    it('returns the correct default fixed size', () => {
+        const codec = getBooleanCodec();
+        expect(codec.fixedSize).toBe(1);
+    });
+
+    it('returns the correct custom fixed size', () => {
+        const codec = getBooleanCodec({ size: getU32Codec() });
+        expect(codec.fixedSize).toBe(4);
+    });
+
+    it('returns the correct custom variable size', () => {
+        const codec = getBooleanCodec({ size: mappedShortU16 });
+        expect(codec.getSizeFromValue(false)).toBe(1);
+        expect(codec.getSizeFromValue(true)).toBe(3);
+        expect(codec.maxSize).toBe(3);
     });
 });
