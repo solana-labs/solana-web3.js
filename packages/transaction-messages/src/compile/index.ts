f3e26f90e0681bb7c45a576d847d11fb8aedcda5
Callum <callum.mcintyre@anza.xyz>
2024-04-05 14:15:56 +0100

    add a function to compile a transaction message
    copied from transactions message.ts

--- packages/transaction-messages/src/compile/index.ts
+++ packages/transaction-messages/src/compile/index.ts
@@ -1,8 +1,9 @@
-// TODO later: can probably remove these exports,
+// TODO later: can probably remove these exports except message,
 // we don't export compile-* from transactions
 export * from './accounts';
 export * from './address-table-lookups';
 export * from './header';
 export * from './instructions';
 export * from './lifetime-token';
+export * from './message';
 export * from './static-accounts';
---
+++ packages/transaction-messages/src/compile/message.ts
@@ -0,0 +1,48 @@
+import { CompilableTransactionMessage } from '../compilable-transaction-message';
+import { getAddressMapFromInstructions, getOrderedAccountsFromAddressMap } from './accounts';
+import { getCompiledAddressTableLookups } from './address-table-lookups';
+import { getCompiledMessageHeader } from './header';
+import { getCompiledInstructions } from './instructions';
+import { getCompiledLifetimeToken } from './lifetime-token';
+import { getCompiledStaticAccounts } from './static-accounts';
+
+type BaseCompiledTransactionMessage = Readonly<{
+    header: ReturnType<typeof getCompiledMessageHeader>;
+    instructions: ReturnType<typeof getCompiledInstructions>;
+    lifetimeToken: ReturnType<typeof getCompiledLifetimeToken>;
+    staticAccounts: ReturnType<typeof getCompiledStaticAccounts>;
+}>;
+
+export type CompiledTransactionMessage = LegacyCompiledTransactionMessage | VersionedCompiledTransactionMessage;
+
+type LegacyCompiledTransactionMessage = BaseCompiledTransactionMessage &
+    Readonly<{
+        version: 'legacy';
+    }>;
+
+type VersionedCompiledTransactionMessage = BaseCompiledTransactionMessage &
+    Readonly<{
+        addressTableLookups?: ReturnType<typeof getCompiledAddressTableLookups>;
+        version: number;
+    }>;
+
+export function newCompileTransactionMessage(
+    transaction: CompilableTransactionMessage & Readonly<{ version: 'legacy' }>,
+): LegacyCompiledTransactionMessage;
+export function newCompileTransactionMessage(
+    transaction: CompilableTransactionMessage,
+): VersionedCompiledTransactionMessage;
+export function newCompileTransactionMessage(transaction: CompilableTransactionMessage): CompiledTransactionMessage {
+    const addressMap = getAddressMapFromInstructions(transaction.feePayer, transaction.instructions);
+    const orderedAccounts = getOrderedAccountsFromAddressMap(addressMap);
+    return {
+        ...(transaction.version !== 'legacy'
+            ? { addressTableLookups: getCompiledAddressTableLookups(orderedAccounts) }
+            : null),
+        header: getCompiledMessageHeader(orderedAccounts),
+        instructions: getCompiledInstructions(transaction.instructions, orderedAccounts),
+        lifetimeToken: getCompiledLifetimeToken(transaction.lifetimeConstraint),
+        staticAccounts: getCompiledStaticAccounts(orderedAccounts),
+        version: transaction.version,
+    };
+}
