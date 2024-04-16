82c57c6e57040c1836154771d7e638d6d30fa6b3
Steven Luscher <steveluscher@users.noreply.github.com>
2024-04-16 14:13:42 -0700

    Use native `WebSocket` in server runtimes that support it (#2500)
    * Use native `WebSocket` in server runtimes that support it
    
    * Create witty-camels-remember.md

--- packages/ws-impl/src/index.node.ts
+++ packages/ws-impl/src/index.node.ts
@@ -1,4 +1,6 @@
 // When building the browser bundle, this import gets replaced by `globalThis.WebSocket`.
 import WebSocketImpl from 'ws';
 
-export default WebSocketImpl;
+export default globalThis.WebSocket
+    ? globalThis.WebSocket // Use native `WebSocket` in runtimes that support it (eg. Deno)
+    : WebSocketImpl;
