12f44cb9081b0bb6526501bc5599f8634683f47e
Callum <callum.mcintyre@anza.xyz>
2024-04-18 11:40:13 +0400

    Rename new-compile-transaction
    - function is already just `compileTransaction`, so just a file rename

--- packages/transactions/src/index.ts
+++ packages/transactions/src/index.ts
@@ -1,6 +1,6 @@
 export * from './codecs';
 export * from './lifetime';
-export * from './new-compile-transaction';
+export * from './compile-transaction';
 export * from './new-signatures';
 export * from './new-wire-transaction';
 export * from './transaction';
