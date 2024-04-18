0bb43f5869fddce261443e9205173f2d62691fc9
Callum <callum.mcintyre@anza.xyz>
2024-04-18 11:58:23 +0400

    Remove OrderedMap export and use SignatureSMap
    Not sure what I was doing here, but OrderedMap is just used for SignaturesMap, and all the uses were just using it as that

--- packages/transactions/src/transaction.ts
+++ packages/transactions/src/transaction.ts
@@ -5,7 +5,7 @@ import { SignatureBytes } from '@solana/keys';
 export type TransactionMessageBytes = ReadonlyUint8Array & { readonly __brand: unique symbol };
 export type TransactionMessageBytesBase64 = string & { readonly __serializedMessageBytesBase64: unique symbol };
 
-export type OrderedMap<K extends string, V> = Record<K, V>;
+type OrderedMap<K extends string, V> = Record<K, V>;
 export type SignaturesMap = OrderedMap<Address, SignatureBytes | null>;
 
 export type Transaction = Readonly<{
