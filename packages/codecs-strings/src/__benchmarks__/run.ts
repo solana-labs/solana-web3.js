7943a4a85e66e05a05858559a6723f286e4d7ee8
Tiberiu Lepadatu <tiberiulepadatu14@gmail.com>
2024-04-24 11:27:35 +0300

    base16 encode benchmark

--- packages/codecs-strings/src/__benchmarks__/run.ts
+++ packages/codecs-strings/src/__benchmarks__/run.ts
@@ -1,9 +1,10 @@
-#!/usr/bin/env -S pnpx tsx
+#!/usr/bin/env -S  pnpm exec tsx
 
 import { webcrypto as crypto } from 'node:crypto';
 
 import { Bench } from 'tinybench';
 
+import { getBase16Codec } from '../base16';
 import { getBase58Codec } from '../base58';
 
 const bench = new Bench({
@@ -11,11 +12,15 @@ const bench = new Bench({
 });
 
 const bytes32 = new Uint8Array(32);
+const bytes16 = new Uint8Array(16);
 let base58EncodedString: string;
+let base16EncodedString: string;
 function randomizeBytes() {
     crypto.getRandomValues(bytes32);
+    crypto.getRandomValues(bytes16);
 }
 const base58Codec = getBase58Codec();
+const base16Codec = getBase16Codec();
 
 bench
     .add(
@@ -36,7 +41,17 @@ bench
                 base58EncodedString = base58Codec.decode(bytes32);
             },
         },
-    );
+    )
+    .add(
+        'Base16 encode',
+        () => {
+            base16Codec.encode(base16EncodedString);
+        }, {
+        beforeEach() {
+            randomizeBytes();
+            base16EncodedString = base16Codec.decode(bytes16);
+        }
+    });
 
 (async () => {
     await bench.warmup();
