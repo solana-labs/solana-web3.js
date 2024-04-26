0aa371750b838104a72e744cbe1d37ca89329c89
TB <tiberiulepadatu14@gmail.com>
2024-04-27 01:56:12 +0300

    [refactor] [experimental] Use optimised base16 encode strategy (#2552)
    * fix #2527
    
    * base16 encode benchmark
    
    * fix pnpx vs pnpm exec
    
    * Performance and code compactness nits
    
    * Run `style:fix`
    
    ---------
    
    Co-authored-by: steveluscher <me+github@steveluscher.com>

--- packages/codecs-strings/src/__benchmarks__/run.ts
+++ packages/codecs-strings/src/__benchmarks__/run.ts
@@ -4,6 +4,7 @@ import { webcrypto as crypto } from 'node:crypto';
 
 import { Bench } from 'tinybench';
 
+import { getBase16Codec } from '../base16';
 import { getBase58Codec } from '../base58';
 
 const bench = new Bench({
@@ -11,11 +12,15 @@ const bench = new Bench({
 });
 
 const bytes32 = new Uint8Array(32);
+const bytes16 = new Uint8Array(16);
 let base58EncodedString: string;
+let base16EncodedString: string;
 function randomizeBytes() {
     crypto.getRandomValues(bytes32);
+    crypto.getRandomValues(bytes16);
 }
 const base58Codec = getBase58Codec();
+const base16Codec = getBase16Codec();
 
 bench
     .add(
@@ -34,6 +39,18 @@ bench
             beforeEach() {
                 randomizeBytes();
                 base58EncodedString = base58Codec.decode(bytes32);
+            },
+        },
+    )
+    .add(
+        'Base16 encode',
+        () => {
+            base16Codec.encode(base16EncodedString);
+        },
+        {
+            beforeEach() {
+                randomizeBytes();
+                base16EncodedString = base16Codec.decode(bytes16);
             },
         },
     );
--- packages/codecs-strings/src/base16.ts
+++ packages/codecs-strings/src/base16.ts
@@ -6,18 +6,63 @@ import {
     VariableSizeDecoder,
     VariableSizeEncoder,
 } from '@solana/codecs-core';
+import { SOLANA_ERROR__CODECS__INVALID_STRING_FOR_BASE, SolanaError } from '@solana/errors';
 
-import { assertValidBaseString } from './assertions';
+const enum HexC {
+    ZERO = 48, // 0
+    NINE = 57, // 9
+    A_UP = 65, // A
+    F_UP = 70, // F
+    A_LO = 97, // a
+    F_LO = 102, // f
+}
+
+const INVALID_STRING_ERROR_BASE_CONFIG = {
+    alphabet: '0123456789abcdef',
+    base: 16,
+} as const;
+
+function charCodeToBase16(char: number) {
+    if (char >= HexC.ZERO && char <= HexC.NINE) return char - HexC.ZERO;
+    if (char >= HexC.A_UP && char <= HexC.F_UP) return char - (HexC.A_UP - 10);
+    if (char >= HexC.A_LO && char <= HexC.F_LO) return char - (HexC.A_LO - 10);
+}
 
 /** Encodes strings in base16. */
 export const getBase16Encoder = (): VariableSizeEncoder<string> =>
     createEncoder({
         getSizeFromValue: (value: string) => Math.ceil(value.length / 2),
         write(value: string, bytes, offset) {
-            const lowercaseValue = value.toLowerCase();
-            assertValidBaseString('0123456789abcdef', lowercaseValue, value);
-            const matches = lowercaseValue.match(/.{1,2}/g);
-            const hexBytes = matches ? matches.map((byte: string) => parseInt(byte, 16)) : [];
+            const len = value.length;
+            const al = len / 2;
+            if (len === 1) {
+                const c = value.charCodeAt(0);
+                const n = charCodeToBase16(c);
+                if (n === undefined) {
+                    throw new SolanaError(SOLANA_ERROR__CODECS__INVALID_STRING_FOR_BASE, {
+                        ...INVALID_STRING_ERROR_BASE_CONFIG,
+                        value,
+                    });
+                }
+                bytes.set([n], offset);
+                return 1 + offset;
+            }
+            const hexBytes = new Uint8Array(al);
+            for (let i = 0, j = 0; i < al; i++) {
+                const c1 = value.charCodeAt(j++);
+                const c2 = value.charCodeAt(j++);
+
+                const n1 = charCodeToBase16(c1);
+                const n2 = charCodeToBase16(c2);
+                if (n1 === undefined || (n2 === undefined && !Number.isNaN(c2))) {
+                    throw new SolanaError(SOLANA_ERROR__CODECS__INVALID_STRING_FOR_BASE, {
+                        ...INVALID_STRING_ERROR_BASE_CONFIG,
+                        value,
+                    });
+                }
+                hexBytes[i] = !Number.isNaN(c2) ? (n1 << 4) | (n2 ?? 0) : n1;
+            }
+
             bytes.set(hexBytes, offset);
             return hexBytes.length + offset;
         },
