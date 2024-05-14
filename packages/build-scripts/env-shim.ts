15017e4844dc7f37478af5a802b580f94f631dcc
Callum <callum.mcintyre@anza.xyz>
2024-05-14 11:40:58 +0100

    make __DEV__ robust to environments without process.env

--- packages/build-scripts/env-shim.ts
+++ packages/build-scripts/env-shim.ts
@@ -1,2 +1,10 @@
-// Clever obfuscation to prevent the build system from inlining the value of `NODE_ENV`
-export const __DEV__ = /* @__PURE__ */ (() => (process as any)['en' + 'v'].NODE_ENV === 'development')();
+function isDevelopment() {
+    if (typeof process === 'object' && process.env) {
+        // We use `'en' + 'v'` to prevent the build system from inlining the value of `NODE_ENV`
+        return (process as any)['en' + 'v'].NODE_ENV === 'development';
+    }
+    // if no process.env, use dev mode
+    return true;
+}
+
+export const __DEV__ = /* @__PURE__ */ isDevelopment();
