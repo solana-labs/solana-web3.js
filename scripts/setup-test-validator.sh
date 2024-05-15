4a6e48ba7e31e0b8898493bfff31b4d4eb783b1e
Steven Luscher <steveluscher@users.noreply.github.com>
2024-05-15 00:35:27 -0700

    Migrate to Agave test validator (#2717)

--- scripts/setup-test-validator.sh
+++ scripts/setup-test-validator.sh
@@ -3,9 +3,9 @@
 set -ex
 
 SCRIPTS_DIR=$(dirname "${BASH_SOURCE[0]}")
-TARGET_DIR=$( cd "$SCRIPTS_DIR/.." ; pwd -P )/.solana
+TARGET_DIR=$( cd "$SCRIPTS_DIR/.." ; pwd -P )/.agave
 
 # setup environment
 version=$(bash $SCRIPTS_DIR/get-latest-validator-release-version.sh)
-sh -c "$(curl -sSfL https://release.solana.com/$version/install)" init $version --data-dir $TARGET_DIR --no-modify-path
+sh -c "$(curl -sSfL https://release.anza.xyz/$version/install)" init $version --data-dir $TARGET_DIR --no-modify-path
 $TARGET_DIR/active_release/bin/solana --version
