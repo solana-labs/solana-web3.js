3e7d4f7369a03f99d19a7ecc26183036ac3e55e5
steveluscher <steven.luscher@anza.xyz>
2024-06-07 01:52:55 +0000

    feat: upgrade to `rpc-websockets` v9

--- packages/library-legacy/src/rpc-websocket-factory.ts
+++
@@ -1,4 +0,0 @@
-import {ICommonWebSocketFactory} from 'rpc-websockets/dist/lib/client/client.types.cjs';
-import WebsocketFactory from 'rpc-websockets/dist/lib/client/websocket.cjs';
-
-export default WebsocketFactory as ICommonWebSocketFactory;
