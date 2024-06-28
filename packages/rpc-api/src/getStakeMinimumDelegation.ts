5b6790ee87164dd1752f94b47a4ad9ebe33fc60f
Joe Caulfield <joe.caulfield@anza.xyz>
2024-06-28 15:47:23 -0500

    refactor(experimental): type reconciliation: `GetStakeMinimumDelegationApi`

--- packages/rpc-api/src/getStakeMinimumDelegation.ts
+++ packages/rpc-api/src/getStakeMinimumDelegation.ts
@@ -1,7 +1,7 @@
 import type { RpcApiMethods } from '@solana/rpc-spec';
 import type { Commitment, LamportsUnsafeBeyond2Pow53Minus1, SolanaRpcResponse } from '@solana/rpc-types';
 
-type GetStakeMinimumDelegationApiResponse = SolanaRpcResponse<LamportsUnsafeBeyond2Pow53Minus1>;
+type GetStakeMinimumDelegationApiResponse = LamportsUnsafeBeyond2Pow53Minus1;
 
 export interface GetStakeMinimumDelegationApi extends RpcApiMethods {
     /**
@@ -11,5 +11,5 @@ export interface GetStakeMinimumDelegationApi extends RpcApiMethods {
         config?: Readonly<{
             commitment?: Commitment;
         }>,
-    ): GetStakeMinimumDelegationApiResponse;
+    ): SolanaRpcResponse<GetStakeMinimumDelegationApiResponse>;
 }
