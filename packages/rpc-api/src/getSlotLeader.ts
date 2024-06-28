c06212a35320624338a95ee363ece1ef79fb985b
Joe Caulfield <joe.caulfield@anza.xyz>
2024-06-28 15:46:24 -0500

    refactor(experimental): type reconciliation: `GetSlotLeaderApi`

--- packages/rpc-api/src/getSlotLeader.ts
+++ packages/rpc-api/src/getSlotLeader.ts
@@ -2,6 +2,8 @@ import type { Address } from '@solana/addresses';
 import type { RpcApiMethods } from '@solana/rpc-spec';
 import type { Commitment, Slot } from '@solana/rpc-types';
 
+type GetSlotLeaderApiResponse = Address;
+
 export interface GetSlotLeaderApi extends RpcApiMethods {
     /**
      * Returns the current slot leader
@@ -11,5 +13,5 @@ export interface GetSlotLeaderApi extends RpcApiMethods {
             commitment?: Commitment;
             minContextSlot?: Slot;
         }>,
-    ): Address;
+    ): GetSlotLeaderApiResponse;
 }
