3c706880e76d3dcf44b5704626f2f53ea1d1cd84
steveluscher <steven.luscher@anza.xyz>
2024-07-18 19:12:51 +0000

    Make example app build/typecheck with rest of CI

--- examples/react-app/.eslintrc.cjs
+++ examples/react-app/.eslintrc.cjs
@@ -3,6 +3,9 @@ module.exports = {
     env: { browser: true, es2020: true },
     extends: ['../../.eslintrc.js', '@solana/eslint-config-solana/react'],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: {
+        project: './tsconfig.app.json',
+    },
     plugins: ['react-refresh'],
     rules: {
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
