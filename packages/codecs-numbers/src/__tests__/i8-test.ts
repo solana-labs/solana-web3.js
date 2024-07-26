3a8e0e7a91dac0fb457cd270e76088b34a48044c
Callum <callum.mcintyre@anza.xyz>
2024-07-26 11:29:49 +0100

    Update i8 encoder to encode bigint | number

--- packages/codecs-numbers/src/__tests__/i8-test.ts
+++ packages/codecs-numbers/src/__tests__/i8-test.ts
@@ -1,5 +1,5 @@
 import { getI8Codec } from '../i8';
-import { assertRangeError, assertValid } from './__setup__';
+import { assertRangeError, assertValid, assertValidEncode } from './__setup__';
 
 const MIN = -Number('0x7f') - 1;
 const MAX = Number('0x7f');
@@ -16,20 +16,26 @@ describe('getI8Codec', () => {
         assertValid(i8(), 0, '00');
         assertValid(i8(), 1, '01');
         assertValid(i8(), 42, '2a');
+        assertValidEncode(i8(), 0n, '00');
+        assertValidEncode(i8(), 1n, '01');
+        assertValidEncode(i8(), 42n, '2a');
         assertValid(i8(), -1, 'ff');
         assertValid(i8(), -42, 'd6');
 
         // Pre-boundaries.
         assertValid(i8(), MIN + 1, '81');
         assertValid(i8(), MAX - 1, '7e');
+        assertValidEncode(i8(), BigInt(MAX - 1), '7e');
 
         // Boundaries.
         assertValid(i8(), MIN, '80');
         assertValid(i8(), MAX, '7f');
+        assertValidEncode(i8(), BigInt(MAX), '7f');
 
         // Out of range.
         assertRangeError(rangeErrorValues, i8(), MIN - 1);
         assertRangeError(rangeErrorValues, i8(), MAX + 1);
+        assertRangeError(rangeErrorValues, i8(), BigInt(MAX + 1));
     });
 
     it('has the right size', () => {
