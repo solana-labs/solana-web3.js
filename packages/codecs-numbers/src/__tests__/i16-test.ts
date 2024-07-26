026beb77baf005eb7ae4840c1249d80eeef2a3ca
Callum <callum.mcintyre@anza.xyz>
2024-07-26 11:33:14 +0100

    Update i16 encoder to encode bigint | number

--- packages/codecs-numbers/src/__tests__/i16-test.ts
+++ packages/codecs-numbers/src/__tests__/i16-test.ts
@@ -1,6 +1,6 @@
 import { Endian } from '../common';
 import { getI16Codec } from '../i16';
-import { assertRangeError, assertValid } from './__setup__';
+import { assertRangeError, assertValid, assertValidEncode } from './__setup__';
 
 const MIN = -Number('0x7fff') - 1;
 const MAX = Number('0x7fff');
@@ -23,6 +23,12 @@ describe('getI16Codec', () => {
         assertValid(i16BE, 1, '0001');
         assertValid(i16LE, 42, '2a00');
         assertValid(i16BE, 42, '002a');
+        assertValidEncode(i16LE, 0n, '0000');
+        assertValidEncode(i16BE, 0n, '0000');
+        assertValidEncode(i16LE, 1n, '0100');
+        assertValidEncode(i16BE, 1n, '0001');
+        assertValidEncode(i16LE, 42n, '2a00');
+        assertValidEncode(i16BE, 42n, '002a');
         assertValid(i16LE, -1, 'ffff');
         assertValid(i16BE, -1, 'ffff');
         assertValid(i16LE, -42, 'd6ff');
@@ -33,18 +39,30 @@ describe('getI16Codec', () => {
         assertValid(i16BE, MIN + 1, '8001');
         assertValid(i16LE, MAX - 1, 'fe7f');
         assertValid(i16BE, MAX - 1, '7ffe');
+        assertValidEncode(i16LE, BigInt(MIN + 1), '0180');
+        assertValidEncode(i16BE, BigInt(MIN + 1), '8001');
+        assertValidEncode(i16LE, BigInt(MAX - 1), 'fe7f');
+        assertValidEncode(i16BE, BigInt(MAX - 1), '7ffe');
 
         // Boundaries.
         assertValid(i16LE, MIN, '0080');
         assertValid(i16BE, MIN, '8000');
         assertValid(i16LE, MAX, 'ff7f');
         assertValid(i16BE, MAX, '7fff');
+        assertValidEncode(i16LE, BigInt(MIN), '0080');
+        assertValidEncode(i16BE, BigInt(MIN), '8000');
+        assertValidEncode(i16LE, BigInt(MAX), 'ff7f');
+        assertValidEncode(i16BE, BigInt(MAX), '7fff');
 
         // Out of range.
         assertRangeError(rangeErrorValues, i16LE, MIN - 1);
         assertRangeError(rangeErrorValues, i16BE, MIN - 1);
         assertRangeError(rangeErrorValues, i16LE, MAX + 1);
         assertRangeError(rangeErrorValues, i16BE, MAX + 1);
+        assertRangeError(rangeErrorValues, i16LE, BigInt(MIN - 1));
+        assertRangeError(rangeErrorValues, i16BE, BigInt(MIN - 1));
+        assertRangeError(rangeErrorValues, i16LE, BigInt(MAX + 1));
+        assertRangeError(rangeErrorValues, i16BE, BigInt(MAX + 1));
     });
 
     it('has the right size', () => {
