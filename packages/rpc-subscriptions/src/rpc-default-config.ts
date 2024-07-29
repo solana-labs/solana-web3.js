036fd5486015449266455910a8eb6f3ce47ea37a
steveluscher <steven.luscher@anza.xyz>
2024-07-29 17:24:58 +0000

    Correct the type of `DEFAULT_RPC_CONFIG`

--- packages/rpc-subscriptions/src/rpc-default-config.ts
+++ packages/rpc-subscriptions/src/rpc-default-config.ts
@@ -2,7 +2,7 @@ import type { createSolanaRpcSubscriptionsApi } from '@solana/rpc-subscriptions-
 
 import { createSolanaJsonRpcIntegerOverflowError } from './rpc-integer-overflow-error';
 
-export const DEFAULT_RPC_CONFIG: Partial<Parameters<typeof createSolanaRpcSubscriptionsApi>[0]> = {
+export const DEFAULT_RPC_CONFIG: Partial<NonNullable<Parameters<typeof createSolanaRpcSubscriptionsApi>[0]>> = {
     defaultCommitment: 'confirmed',
     onIntegerOverflow(methodName, keyPath, value) {
         throw createSolanaJsonRpcIntegerOverflowError(methodName, keyPath, value);
