09d92863ab4fce5a34634ea29918d94cb557624b
Loris Leiva <loris.leiva@gmail.com>
2024-08-23 13:37:05 +0100

    Move types outside of `rpc-request` file

--- packages/rpc-spec/src/rpc-api.ts
+++ packages/rpc-spec/src/rpc-api.ts
@@ -1,11 +1,15 @@
 import { Callable } from '@solana/rpc-spec-types';
 
-import { RpcApiRequestPlan } from './rpc-request';
-
 export type RpcApiConfig = Readonly<{
     parametersTransformer?: <T extends unknown[]>(params: T, methodName: string) => unknown;
     responseTransformer?: <T>(response: unknown, methodName?: string) => T;
 }>;
+
+export type RpcApiRequestPlan<TResponse> = {
+    methodName: string;
+    params: unknown;
+    responseTransformer?: (response: unknown, methodName: string) => TResponse;
+};
 
 export type RpcApi<TRpcMethods> = {
     [MethodName in keyof TRpcMethods]: RpcReturnTypeMapper<TRpcMethods[MethodName]>;
