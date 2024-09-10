31aa1a0700e0683919f72f6f757b11a2554fb83e
Loris Leiva <loris.leiva@gmail.com>
2024-09-10 09:50:06 +0100

    Extract `isJsonRpcPayload` method to `rpc-spec`

---
+++ .changeset/bright-snakes-search.md
@@ -0,0 +1,5 @@
+---
+'@solana/rpc-spec': patch
+---
+
+Add new `isJsonRpcPayload` helper method
--- packages/rpc-spec/src/rpc-transport.ts
+++ packages/rpc-spec/src/rpc-transport.ts
@@ -8,3 +8,20 @@ type RpcTransportRequest = Readonly<{
 export type RpcTransport = {
     <TResponse>(request: RpcTransportRequest): Promise<RpcResponse<TResponse>>;
 };
+
+export function isJsonRpcPayload(payload: unknown): payload is Readonly<{
+    jsonrpc: '2.0';
+    method: string;
+    params: unknown;
+}> {
+    if (payload == null || typeof payload !== 'object' || Array.isArray(payload)) {
+        return false;
+    }
+    return (
+        'jsonrpc' in payload &&
+        payload.jsonrpc === '2.0' &&
+        'method' in payload &&
+        typeof payload.method === 'string' &&
+        'params' in payload
+    );
+}
