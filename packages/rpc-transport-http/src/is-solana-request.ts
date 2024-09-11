89a25aa194bcdadad20811b1c3278711d7ea7890
Loris Leiva <loris.leiva@gmail.com>
2024-09-11 09:24:31 +0100

    Add `isSolanaRequest` helper (#3195)
    This PR adds a new `isSolanaRequest` helper function that checks if a given `RpcRequest` comes from the Solana RPC API.
    
    This will be used — in a subsequent PR — to create a new Solana-RPC-specific HTTP transport that prevents loss of precision for large integers.

---
+++ packages/rpc-transport-http/src/is-solana-request.ts
@@ -0,0 +1,69 @@
+import { isJsonRpcPayload } from '@solana/rpc-spec';
+
+const SOLANA_RPC_METHODS = [
+    'getAccountInfo',
+    'getBalance',
+    'getBlock',
+    'getBlockCommitment',
+    'getBlockHeight',
+    'getBlockProduction',
+    'getBlocks',
+    'getBlocksWithLimit',
+    'getBlockTime',
+    'getClusterNodes',
+    'getEpochInfo',
+    'getEpochSchedule',
+    'getFeeForMessage',
+    'getFirstAvailableBlock',
+    'getGenesisHash',
+    'getHealth',
+    'getHighestSnapshotSlot',
+    'getIdentity',
+    'getInflationGovernor',
+    'getInflationRate',
+    'getInflationReward',
+    'getLargestAccounts',
+    'getLatestBlockhash',
+    'getLeaderSchedule',
+    'getMaxRetransmitSlot',
+    'getMaxShredInsertSlot',
+    'getMinimumBalanceForRentExemption',
+    'getMultipleAccounts',
+    'getProgramAccounts',
+    'getRecentPerformanceSamples',
+    'getRecentPrioritizationFees',
+    'getSignaturesForAddress',
+    'getSignatureStatuses',
+    'getSlot',
+    'getSlotLeader',
+    'getSlotLeaders',
+    'getStakeActivation',
+    'getStakeMinimumDelegation',
+    'getSupply',
+    'getTokenAccountBalance',
+    'getTokenAccountsByDelegate',
+    'getTokenAccountsByOwner',
+    'getTokenLargestAccounts',
+    'getTokenSupply',
+    'getTransaction',
+    'getTransactionCount',
+    'getVersion',
+    'getVoteAccounts',
+    'index',
+    'isBlockhashValid',
+    'minimumLedgerSlot',
+    'requestAirdrop',
+    'sendTransaction',
+    'simulateTransaction',
+] as const;
+
+/**
+ * Helper function that checks if a given `RpcRequest` comes from the Solana RPC API.
+ */
+export function isSolanaRequest(payload: unknown): payload is Readonly<{
+    jsonrpc: '2.0';
+    method: (typeof SOLANA_RPC_METHODS)[number];
+    params: unknown;
+}> {
+    return isJsonRpcPayload(payload) && (SOLANA_RPC_METHODS as readonly string[]).includes(payload.method);
+}
