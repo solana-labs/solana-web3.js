0d9069ad8e5b4a5f100eda38e51cde2b285b43c1
Steven Luscher <steven.luscher@anza.xyz>
2024-10-02 21:57:09 +0000

    A channel augmenter that encodes and decodes messages as JSON

---
+++ packages/rpc-subscriptions/src/__tests__/rpc-subscriptions-json-test.ts
@@ -0,0 +1,30 @@
+import { RpcSubscriptionsChannel } from '@solana/rpc-subscriptions-spec';
+
+import { getRpcSubscriptionsChannelWithJSONSerialization } from '../rpc-subscriptions-json';
+
+describe('getRpcSubscriptionsChannelWithJSONSerialization', () => {
+    let mockOn: jest.Mock;
+    let channel: RpcSubscriptionsChannel<string, string>;
+    function receiveMessage(message: unknown) {
+        mockOn.mock.calls.filter(([type]) => type === 'message').forEach(([_, listener]) => listener(message));
+    }
+    beforeEach(() => {
+        mockOn = jest.fn();
+        channel = {
+            on: mockOn,
+            send: jest.fn(),
+        };
+    });
+    it('forwards JSON-serialized messages to the underlying channel', () => {
+        const channelWithJSONSerialization = getRpcSubscriptionsChannelWithJSONSerialization(channel);
+        channelWithJSONSerialization.send('hello');
+        expect(channel.send).toHaveBeenCalledWith(JSON.stringify('hello'));
+    });
+    it('deserializes messages received from the underlying channel as JSON', () => {
+        const channelWithJSONSerialization = getRpcSubscriptionsChannelWithJSONSerialization(channel);
+        const messageListener = jest.fn();
+        channelWithJSONSerialization.on('message', messageListener);
+        receiveMessage(JSON.stringify('hello'));
+        expect(messageListener).toHaveBeenCalledWith('hello');
+    });
+});
---
+++ packages/rpc-subscriptions/src/rpc-subscriptions-json.ts
@@ -0,0 +1,26 @@
+import { RpcSubscriptionsChannel } from '@solana/rpc-subscriptions-spec';
+
+export function getRpcSubscriptionsChannelWithJSONSerialization(
+    channel: RpcSubscriptionsChannel<string, string>,
+): RpcSubscriptionsChannel<unknown, unknown> {
+    return Object.freeze({
+        ...channel,
+        on(type, listener, options) {
+            if (type !== 'message') {
+                return channel.on(type, listener, options);
+            }
+            return channel.on(
+                'message',
+                function deserializingListener(message: string) {
+                    const deserializedMessage = JSON.parse(message);
+                    listener(deserializedMessage);
+                },
+                options,
+            );
+        },
+        send(message) {
+            const serializedMessage = JSON.stringify(message);
+            return channel.send(serializedMessage);
+        },
+    } as RpcSubscriptionsChannel<unknown, unknown>);
+}
