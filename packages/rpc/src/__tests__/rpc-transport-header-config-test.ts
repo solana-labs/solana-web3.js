96623536dffa1fa1140d09b7b8c500fe19ee5ba2
Steven Luscher <steven.luscher@anza.xyz>
2024-10-06 04:02:02 +0000

    The default RPC transport now requests wire compression in Node

--- packages/rpc/src/__tests__/rpc-transport-header-config-test.ts
+++ packages/rpc/src/__tests__/rpc-transport-header-config-test.ts
@@ -5,6 +5,31 @@ import { createDefaultRpcTransport } from '../rpc-transport';
 jest.mock('@solana/rpc-transport-http');
 
 describe('createDefaultRpcTransport', () => {
+    if (__NODEJS__) {
+        it('adds default compression headers', () => {
+            createDefaultRpcTransport({ url: 'fake://url' });
+            expect(createHttpTransportForSolanaRpc).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    headers: expect.objectContaining({
+                        'accept-encoding': 'br,gzip,deflate',
+                    }),
+                }),
+            );
+        });
+        it('allows the override of the default compression headers', () => {
+            createDefaultRpcTransport({
+                headers: { 'aCcEpT-eNcOdInG': 'zstd' },
+                url: 'fake://url',
+            });
+            expect(createHttpTransportForSolanaRpc).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    headers: expect.objectContaining({
+                        'accept-encoding': 'zstd',
+                    }),
+                }),
+            );
+        });
+    }
     it('adds configured headers to each request with downcased property names', () => {
         createDefaultRpcTransport({
             headers: { aUtHoRiZaTiOn: 'Picard, 4 7 Alpha Tango' },
