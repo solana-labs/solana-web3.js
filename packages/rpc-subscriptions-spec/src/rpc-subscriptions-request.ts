0f739b64de5f5d2d23d24ad6a1d0508e3ae37e20
Loris Leiva <loris.leiva@gmail.com>
2024-10-23 09:45:34 +0100

    Remove unused RpcSubscriptionsRequest type (#3391)
    This PR removes the `RpcSubscriptionsRequest` which was used in the previous implementation of RPC Subscriptions but no longer is.

--- packages/rpc-subscriptions-spec/src/rpc-subscriptions-request.ts
+++ packages/rpc-subscriptions-spec/src/rpc-subscriptions-request.ts
@@ -1,10 +1,3 @@
-export type RpcSubscriptionsRequest<TResponse> = {
-    params: unknown[];
-    responseTransformer?: (response: unknown, notificationName: string) => TResponse;
-    subscribeMethodName: string;
-    unsubscribeMethodName: string;
-};
-
 export type PendingRpcSubscriptionsRequest<TNotification> = {
     subscribe(options: RpcSubscribeOptions): Promise<AsyncIterable<TNotification>>;
 };
