ba0a45526e33ba90037b56c381f3217d04b7a19d
Callum <callum.mcintyre@anza.xyz>
2024-10-31 15:43:26 +0000

    Add a failing test that verifies we can subscribe to an RPC subscription method

---
+++ packages/rpc-subscriptions/src/__tests__/rpc-subscriptions-functional-test.ts
@@ -0,0 +1,40 @@
+import { Address } from '@solana/addresses';
+import { AccountNotificationsApi, SolanaRpcSubscriptionsApi } from '@solana/rpc-subscriptions-api';
+import { createSubscriptionRpc, RpcSubscriptions } from '@solana/rpc-subscriptions-spec';
+
+import {
+    createDefaultRpcSubscriptionsTransport,
+    createDefaultSolanaRpcSubscriptionsChannelCreator,
+    createSolanaRpcSubscriptionsApi,
+} from '..';
+
+function createLocalhostSolanaRpcSubscriptions(): RpcSubscriptions<SolanaRpcSubscriptionsApi> {
+    return createSubscriptionRpc({
+        api: createSolanaRpcSubscriptionsApi(),
+        transport: createDefaultRpcSubscriptionsTransport({
+            createChannel: createDefaultSolanaRpcSubscriptionsChannelCreator({ url: 'ws://localhost:8900' }),
+        }),
+    });
+}
+
+describe('accountNotifications', () => {
+    let rpcSubscriptions: RpcSubscriptions<AccountNotificationsApi>;
+    beforeEach(() => {
+        rpcSubscriptions = createLocalhostSolanaRpcSubscriptions();
+    });
+
+    // eslint-disable-next-line jest/no-disabled-tests
+    it.skip('can subscribe to account notifications', async () => {
+        expect.hasAssertions();
+        const abortSignal = new AbortController().signal;
+        const subscriptionPromise = rpcSubscriptions
+            .accountNotifications('4nTLDQiSTRHbngKZWPMfYnZdWTbKiNeuuPcX7yFUpSAc' as Address)
+            .subscribe({ abortSignal });
+
+        await expect(subscriptionPromise).resolves.toEqual(
+            expect.objectContaining({
+                [Symbol.asyncIterator]: expect.any(Function),
+            }),
+        );
+    });
+});
